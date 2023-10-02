24718407e50d20dc1cb832c2a0445ae994f41719
Slaven Peles <peless@ornl.gov>
2023-10-02 12:46:46 -0400

    Eliminate input files from unit tests.

--- tests/unit/matrix/runMatrixIoTests.cpp
+++ tests/unit/matrix/runMatrixIoTests.cpp
@@ -12,23 +12,11 @@ int main(int argc, char* argv[])
   // Input to this code is location of `data` directory where matrix files are stored
   const std::string data_path = (argc == 2) ? argv[1] : "./";
 
-  std::string matrixFileName = data_path + "matrix/data/matrix_general_coo_ordered.mtx";
-  // std::cout << "Opening file " << matrixFileName << " ...\n";
-  std::ifstream file(matrixFileName);
-  if(!file.is_open())
-  {
-    std::cout << "Failed to open file ...\n";
-    return -1;
-  }
-
   ReSolve::tests::TestingResults result;
   result += test.cooMatrixImport();
-  result += test.cooMatrixImport2(file);
   result += test.cooMatrixReadAndUpdate();
   result += test.rhsVectorReadFromFile();
   result += test.rhsVectorReadAndUpdate();
-
-  file.close();
 
   return result.summary();
 }
