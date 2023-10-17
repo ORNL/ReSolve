7944ab5a2e45172c55ae0443dd271efb4c89c1ee
spack-auto-module <spack.bot@no-reply.com>
2023-10-17 14:54:53 -0400

    Update Ascent spack built tcl modules

--- buildsystem/spack/ascent/modules/dependencies.sh
+++ buildsystem/spack/ascent/modules/dependencies.sh
@@ -1,6 +1,6 @@
-module use -a /gpfs/wolf/proj-shared/csc359/resolve/spack-ci/install/modules/linux-rhel8-power9le
+module use -a /gpfs/wolf/proj-shared/csc359/resolve/github-ci/install/modules/linux-rhel8-power9le
 # cmake@=3.22.2%gcc@=10.2~doc+ncurses+ownlibs build_system=generic build_type=Release arch=linux-rhel8-power9le
-module load cmake/3.22.2-gcc-10.2-licrwny
+module load cmake/3.22.2-gcc-10.2-jukgoau
 # cuda@=11.4.2%gcc@=10.2~allow-unsupported-compilers~dev build_system=generic arch=linux-rhel8-power9le
 module load cuda/11.4.2-gcc-10.2-vlwmnfb
 # gnuconfig@=2022-09-17%gcc@=10.2 build_system=generic arch=linux-rhel8-power9le
@@ -9,5 +9,5 @@ module load gnuconfig/2022-09-17-gcc-10.2-ujor7tm
 module load gmake/4.4.1-gcc-10.2-rnqfmzf
 # suite-sparse@=5.10.1%gcc@=10.2~cuda~graphblas~openmp+pic~tbb build_system=generic arch=linux-rhel8-power9le
 module load suite-sparse/5.10.1-gcc-10.2-otp7tib
-# resolve@=develop%gcc@=10.2+cuda~ipo+klu build_system=cmake build_type=Release cuda_arch=70 dev_path=/gpfs/wolf/proj-shared/csc359/ci/466546 generator=make arch=linux-rhel8-power9le
-## module load resolve/develop-gcc-10.2-66cvnuz
+# resolve@=develop%gcc@=10.2+cuda~ipo+klu build_system=cmake build_type=Release cuda_arch=70 dev_path=/gpfs/wolf/proj-shared/csc359/ci/469618 generator=make arch=linux-rhel8-power9le
+## module load resolve/develop-gcc-10.2-bg35urs
