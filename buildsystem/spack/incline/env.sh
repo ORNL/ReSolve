df771b3875da75a95ba21edb380d6ea141c06580
rcrutherford <robert.rutherford@pnnl.gov>
2023-10-31 11:27:15 -0700

    Add working incline build

--- buildsystem/spack/incline/env.sh
+++ buildsystem/spack/incline/env.sh
@@ -7,14 +7,18 @@ module purge
 module load python/miniconda4.12
 source /share/apps/python/miniconda4.12/etc/profile.d/conda.sh
 
+export HTTPS_PROXY=http://proxy01.pnl.gov:3128
+export https_proxy=http://proxy01.pnl.gov:3128
+
 # Define environment variables for where spack stores key files
 # For now, SPACK_INSTALL is the path where everything spack related is installed
 # If you want to modify the module install path, edit the spack.yaml manually
 BASE=/qfs/projects/exasgd/resolve/spack-ci
 export SPACK_INSTALL=$BASE/install
+export SPACK_MIRROR=$BASE/../$(whoami)/spack-mirror
 export SPACK_CACHE=$BASE/../$(whoami)/spack-cache
 export SPACK_DISABLE_LOCAL_CONFIG=1
-export SPACK_PYTHON=$(which python)
+export SPACK_PYTHON=$(which python3)
 
 export tempdir=$SPACK_CACHE
 export TMP=$SPACK_CACHE
