be94ee749ea84780099de4caaef2d12a9f5c5579
rcrutherford <robert.rutherford@pnnl.gov>
2023-10-31 12:55:22 -0700

    Add first pass CMake cleanup - need to fix include_directories and add custom library macro.

--- cmake/ReSolveFindHipLibraries.cmake
+++ cmake/ReSolveFindHipLibraries.cmake
@@ -14,10 +14,4 @@ target_link_libraries(resolve_hip INTERFACE
   #roc::hipblas
 )
 
-# get_target_property(hip_includes hip::device INTERFACE_INCLUDE_DIRECTORIES)
-# message(STATUS "HIP include directories: ${hip_includes}")
-
-# get_target_property(resolve_hip_includes resolve_hip INTERFACE_INCLUDE_DIRECTORIES)
-# message(STATUS "ReSolve HIP include directories: ${resolve_hip_includes}")
-
 install(TARGETS resolve_hip EXPORT ReSolveTargets)
