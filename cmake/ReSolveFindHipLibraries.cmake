2d8ce8bc966a1dc49c3a802e92760e5883a16515
Slaven Peles <peless@ornl.gov>
2023-11-01 13:07:48 -0400

    Fix find HIP cmake function.

--- cmake/ReSolveFindHipLibraries.cmake
+++ cmake/ReSolveFindHipLibraries.cmake
@@ -4,14 +4,14 @@
 add_library(resolve_hip INTERFACE)
 
 find_package(hip REQUIRED)
-find_package(hipblas REQUIRED)
+find_package(rocblas REQUIRED)
+find_package(rocsparse REQUIRED)
 
 target_link_libraries(resolve_hip INTERFACE
-  #hip::host 
+  hip::host 
   hip::device
-  rocblas
-  rocsparse
-  #roc::hipblas
+  roc::rocblas
+  roc::rocsparse
 )
 
 install(TARGETS resolve_hip EXPORT ReSolveTargets)
