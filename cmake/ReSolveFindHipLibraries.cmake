9a5fd7adba72170f40a0e91c87171dc9c1ae3659
pelesh <peless@ornl.gov>
2023-11-01 13:51:40 -0400

    Review CUDA and HIP configuration in CMake (#48)
    * Add first pass CMake cleanup - need to fix include_directories and add custom library macro.
    
    * Fix find HIP cmake function.
    
    * Update CMakePresets.json
    
    Co-authored-by: Nicholson Koukpaizan <72402802+nkoukpaizan@users.noreply.github.com>
    
    
    ---------
    
    Co-authored-by: rcrutherford <robert.rutherford@pnnl.gov>
    Co-authored-by: Nicholson Koukpaizan <72402802+nkoukpaizan@users.noreply.github.com>

--- cmake/ReSolveFindHipLibraries.cmake
+++ cmake/ReSolveFindHipLibraries.cmake
@@ -4,20 +4,14 @@
 add_library(resolve_hip INTERFACE)
 
 find_package(hip REQUIRED)
-find_package(hipblas REQUIRED)
+find_package(rocblas REQUIRED)
+find_package(rocsparse REQUIRED)
 
 target_link_libraries(resolve_hip INTERFACE
-  #hip::host 
+  hip::host 
   hip::device
-  rocblas
-  rocsparse
-  #roc::hipblas
+  roc::rocblas
+  roc::rocsparse
 )
-
-# get_target_property(hip_includes hip::device INTERFACE_INCLUDE_DIRECTORIES)
-# message(STATUS "HIP include directories: ${hip_includes}")
-
-# get_target_property(resolve_hip_includes resolve_hip INTERFACE_INCLUDE_DIRECTORIES)
-# message(STATUS "ReSolve HIP include directories: ${resolve_hip_includes}")
 
 install(TARGETS resolve_hip EXPORT ReSolveTargets)
