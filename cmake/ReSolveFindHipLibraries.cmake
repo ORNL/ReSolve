9085e914894e9d3233a2ff531e9fe1d0f4f45d70
rcrutherford <robert.rutherford@pnnl.gov>
2023-11-02 14:53:17 -0700

    Update spack and add rocsolver/roctracer to CMake.

--- cmake/ReSolveFindHipLibraries.cmake
+++ cmake/ReSolveFindHipLibraries.cmake
@@ -6,13 +6,15 @@ add_library(resolve_hip INTERFACE)
 find_package(hip REQUIRED)
 find_package(rocblas REQUIRED)
 find_package(rocsparse REQUIRED)
+find_package(rocsolver REQUIRED)
 
 target_link_libraries(resolve_hip INTERFACE
   hip::host 
   hip::device
   roc::rocblas
   roc::rocsparse
-  rocsolver
+  roc::rocsolver
 )
 
 install(TARGETS resolve_hip EXPORT ReSolveTargets)
+
