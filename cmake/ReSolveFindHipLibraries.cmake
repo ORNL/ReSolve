0dc6b01af66fdc462c0403acf1eb3d0986b3fe31
rcrutherford <robert.rutherford@pnnl.gov>
2023-11-02 17:53:07 -0700

    Incline working changes

--- cmake/ReSolveFindHipLibraries.cmake
+++ cmake/ReSolveFindHipLibraries.cmake
@@ -8,13 +8,18 @@ find_package(rocblas REQUIRED)
 find_package(rocsparse REQUIRED)
 find_package(rocsolver REQUIRED)
 
-target_link_libraries(resolve_hip INTERFACE
+target_link_libraries(resolve_hip INTERFACE 
   hip::host 
   hip::device
   roc::rocblas
   roc::rocsparse
   roc::rocsolver
 )
+
+get_target_property(hip_includes hip::device INTERFACE_INCLUDE_DIRECTORIES)
+
+target_include_directories(resolve_hip INTERFACE 
+  $<BUILD_INTERFACE:${hip_includes}>)
 
 install(TARGETS resolve_hip EXPORT ReSolveTargets)
 
