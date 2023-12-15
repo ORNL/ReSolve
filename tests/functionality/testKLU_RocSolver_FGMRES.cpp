5c636468156260eb8b019ae58793d5d49c2a8b45
kswirydo <kasia.swirydowicz@gmail.com>
2023-12-15 17:03:33 -0500

    fixing solve(rhs) vs solve(x, rhs) discrepancy (#119)
    * fixing solve(rhs) vs solve(x, rhs) discrepancy
    
    ---------
    
    Co-authored-by: Slaven Peles <peless@ornl.gov>

--- tests/functionality/testKLU_RocSolver_FGMRES.cpp
+++ tests/functionality/testKLU_RocSolver_FGMRES.cpp
@@ -197,8 +197,7 @@ int main(int argc, char *argv[])
   error_sum += status;
   
   vec_x->update(rhs, ReSolve::memory::HOST, ReSolve::memory::DEVICE);
-  // TODO: Investigate why results are different when using Rf->solve(vec_x) !!
-  status = Rf->solve(vec_rhs, vec_x);
+  status = Rf->solve(vec_x);
   error_sum += status;
   
   FGMRES->resetMatrix(A);
