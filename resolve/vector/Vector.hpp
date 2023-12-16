92b01e7c102f6d250929301b4bee1c30ea80e9e1
kswirydo <kasia.swirydowicz@gmail.com>
2023-12-15 22:40:30 -0500

    Vector and Sparse classes doxygen documentation

--- resolve/vector/Vector.hpp
+++ resolve/vector/Vector.hpp
@@ -4,6 +4,19 @@
 #include <resolve/MemoryUtils.hpp>
 
 namespace ReSolve { namespace vector {
+  /**
+   * @brief This class implements vectors (dense arrays) and multivectors and some basic utilities (get size, allocate, set data, get data, etc).
+   *       
+   * 
+   * What you need to know:
+   *  - Multivectors are stored in one array, organized column-wise. A vector is a multivector of size 1.
+   *  - There is a mirroring memory approach: the class has DEVICE and HOST data pointers. If needed,  only one (or none) can be used or allocated. Unless triggered directly or by other function, the data is NOT automatically updated between HOST and DEVICE.
+   *  - Constructor DOES NOT allocate memory. This has to be done separately.
+   *  - You can get (and set) "raw" data easily, if needed. 
+   *  - There is memory ownership utility - vector can own memory (separate flags for HOST and DEVICE) or not, depending on how it is used.
+   *
+   * @author Kasia Swirydowicz <kasia.swirydowicz@pnnl.gov>
+   */
   class Vector 
   {
     public:
@@ -36,14 +49,14 @@ namespace ReSolve { namespace vector {
     private:
       index_type n_; ///< size
       index_type k_; ///< k_ = 1 for vectors and k_>1 for multivectors (multivectors are accessed column-wise). 
-      index_type n_current_; // if vectors dynamically change size, "current n_" keeps track of this. Needed for some solver implementations. 
-      real_type* d_data_{nullptr};
-      real_type* h_data_{nullptr};
-      bool gpu_updated_;
-      bool cpu_updated_;
+      index_type n_current_; ///< if vectors dynamically changes size, "current n_" keeps track of this. Needed for some solver implementations. 
+      real_type* d_data_{nullptr}; ///< DEVICE data array
+      real_type* h_data_{nullptr}; ///< HOST data array
+      bool gpu_updated_; ///< DEVICE data flag (updated or not)
+      bool cpu_updated_; ///< HOST data flag (updated or not)
 
-      bool owns_gpu_data_{false};
-      bool owns_cpu_data_{false};
+      bool owns_gpu_data_{false}; ///< data owneship flag for DEVICE data
+      bool owns_cpu_data_{false}; ///< data ownership flag for HOST data
 
       MemoryHandler mem_; ///< Device memory manager object
   };
