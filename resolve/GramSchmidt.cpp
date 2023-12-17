af11f5bfb639f41e0d138c92919d37804e09979e
Slaven Peles <peless@ornl.gov>
2023-12-16 19:41:35 -0500

    Use memspace as parameter consistently in FGMRES classes.

--- resolve/GramSchmidt.cpp
+++ resolve/GramSchmidt.cpp
@@ -110,212 +110,212 @@ namespace ReSolve
     return 0;
   }
 
-  //this always happen on the GPU
   int GramSchmidt::orthogonalize(index_type n, vector::Vector* V, real_type* H, index_type i, memory::MemorySpace memspace)
   {
     using namespace constants;
 
-    if (memspace == memory::DEVICE) {
-
-      double t;
-      double s;
-
-      switch (variant_){
-        case mgs: 
-
-          vec_w_->setData(V->getVectorData(i + 1, memory::DEVICE), memory::DEVICE);
-          for(int j = 0; j <= i; ++j) {
-            t = 0.0;
-            vec_v_->setData( V->getVectorData(j, memory::DEVICE), memory::DEVICE);
-            t = vector_handler_->dot(vec_v_, vec_w_, memspace);  
-            H[ idxmap(i, j, num_vecs_ + 1) ] = t; 
-            t *= -1.0;
-            vector_handler_->axpy(&t, vec_v_, vec_w_, memspace);  
-          }
-          t = 0.0;
-          t = vector_handler_->dot(vec_w_, vec_w_, memspace);
-          //set the last entry in Hessenberg matrix
-          t = sqrt(t);
-          H[ idxmap(i, i + 1, num_vecs_ + 1) ] = t; 
-          if(fabs(t) > EPSILON) {
-            t = 1.0/t;
-            vector_handler_->scal(&t, vec_w_, memspace);  
-          } else {
-            assert(0 && "Gram-Schmidt failed, vector with ZERO norm\n");
-            return -1;
-          }
-          break;
-        case cgs2:
-
-          vec_v_->setData(V->getVectorData(i + 1, memory::DEVICE), memory::DEVICE);
-          vector_handler_->gemv('T', n, i + 1, &ONE, &ZERO, V,  vec_v_, vec_Hcolumn_, memspace);
-          // V(:,i+1) = V(:, i+1) -  V(:,1:i)*Hcol
-          vector_handler_->gemv('N', n, i + 1, &ONE, &MINUSONE, V, vec_Hcolumn_, vec_v_, memspace );  
-          mem_.deviceSynchronize();
-          
-          // copy H_col to aux, we will need it later
-          vec_Hcolumn_->setDataUpdated(memory::DEVICE);
-          vec_Hcolumn_->setCurrentSize(i + 1);
-          vec_Hcolumn_->deepCopyVectorData(h_aux_, 0, memory::HOST);
-          mem_.deviceSynchronize();
-
-          //Hcol = V(:,1:i)^T*V(:,i+1);
-          vector_handler_->gemv('T', n, i + 1, &ONE, &ZERO, V,  vec_v_, vec_Hcolumn_, memspace);
-          mem_.deviceSynchronize();
-
-          // V(:,i+1) = V(:, i+1) -  V(:,1:i)*Hcol
-          vector_handler_->gemv('N', n, i + 1, &ONE, &MINUSONE, V, vec_Hcolumn_, vec_v_, memspace );  
-          mem_.deviceSynchronize();
-
-          // copy H_col to H
-          vec_Hcolumn_->setDataUpdated(memory::DEVICE);
-          vec_Hcolumn_->deepCopyVectorData(&H[ idxmap(i, 0, num_vecs_ + 1)], 0, memory::HOST);
-          mem_.deviceSynchronize();
-
-          // add both pieces together (unstable otherwise, careful here!!)
-          t = 0.0;
-          for(int j = 0; j <= i; ++j) {
-            H[ idxmap(i, j, num_vecs_ + 1)] += h_aux_[j];
-          }
-
-          t = vector_handler_->dot(vec_v_, vec_v_, memspace);  
-          //set the last entry in Hessenberg matrix
-          t = sqrt(t);
-          H[ idxmap(i, i + 1, num_vecs_ + 1) ] = t; 
-
-          if(fabs(t) > EPSILON) {
-            t = 1.0/t;
-            vector_handler_->scal(&t, vec_v_, memspace);  
-          } else {
-            assert(0 && "Gram-Schmidt failed, vector with ZERO norm\n");
-            return -1;
-          }
-          return 0;
-          break;
-        case mgs_two_synch:
-          // V[1:i]^T[V[i] w]
-          vec_v_->setData(V->getVectorData(i, memory::DEVICE), memory::DEVICE);
-          vec_w_->setData(V->getVectorData(i + 1, memory::DEVICE), memory::DEVICE);
-          vec_rv_->setCurrentSize(i + 1);
-
-          vector_handler_->massDot2Vec(n, V, i, vec_v_, vec_rv_, memspace);
-          vec_rv_->setDataUpdated(memory::DEVICE);
-          vec_rv_->copyData(memory::DEVICE, memory::HOST);
-
-          vec_rv_->deepCopyVectorData(&h_L_[idxmap(i, 0, num_vecs_ + 1)], 0, memory::HOST);
-          h_rv_ = vec_rv_->getVectorData(1, memory::HOST);
-
-          for(int j=0; j<=i; ++j) {
-            H[ idxmap(i, j, num_vecs_ + 1) ] = 0.0;
-          }
-          // triangular solve
-          for(int j = 0; j <= i; ++j) {
-            H[ idxmap(i, j, num_vecs_ + 1) ] = h_rv_[j];
-            s = 0.0;
-            for(int k = 0; k < j; ++k) {
-              s += h_L_[ idxmap(j, k, num_vecs_ + 1) ] * H[ idxmap(i, k, num_vecs_ + 1) ];
-            } // for k
-            H[ idxmap(i, j, num_vecs_ + 1) ] -= s; 
-          }   // for j
-          vec_Hcolumn_->setCurrentSize(i + 1);
-          vec_Hcolumn_->update(&H[ idxmap(i, 0, num_vecs_ + 1)], memory::HOST, memory::DEVICE); 
-          vector_handler_->massAxpy(n, vec_Hcolumn_, i, V, vec_w_, memspace);
-
-          // normalize (second synch)
-          t = vector_handler_->dot(vec_w_, vec_w_, memspace);  
-          //set the last entry in Hessenberg matrix
-          t = sqrt(t);
-          H[ idxmap(i, i + 1, num_vecs_ + 1)] = t;    
-          if(fabs(t) > EPSILON) {
-            t = 1.0 / t;
-            vector_handler_->scal(&t, vec_w_, memspace);  
-          } else {
-            assert(0 && "Iterative refinement failed, Krylov vector with ZERO norm\n");
-            return -1;
-          }
-          return 0;
-          break;
-        case mgs_pm:
-          vec_v_->setData(V->getVectorData(i, memory::DEVICE), memory::DEVICE);
-          vec_w_->setData(V->getVectorData(i + 1, memory::DEVICE), memory::DEVICE);
-          vec_rv_->setCurrentSize(i + 1);
-
-          vector_handler_->massDot2Vec(n, V, i, vec_v_, vec_rv_, memspace);
-          vec_rv_->setDataUpdated(memory::DEVICE);
-          vec_rv_->copyData(memory::DEVICE, memory::HOST);
-
-          vec_rv_->deepCopyVectorData(&h_L_[idxmap(i, 0, num_vecs_ + 1)], 0, memory::HOST);
-          h_rv_ = vec_rv_->getVectorData(1, memory::HOST);
-
-          for(int j = 0; j <= i; ++j) {
-            H[ idxmap(i, j, num_vecs_ + 1) ] = 0.0;
-          }
-
-          //triangular solve
-          for(int j = 0; j <= i; ++j) {
-            H[ idxmap(i, j, num_vecs_ + 1) ] = h_rv_[j];
-            s = 0.0;
-            for(int k = 0; k < j; ++k) {
-              s += h_L_[ idxmap(j, k, num_vecs_ + 1) ] * H[ idxmap(i, k, num_vecs_ + 1) ];
-            } // for k
-            H[ idxmap(i, j, num_vecs_ + 1) ] -= s;
-          }   // for j
-
-          // now compute h_rv = L^T h_H
-          double h;
-          for(int j = 0; j <= i; ++j) {
-            // go through COLUMN OF L
-            h_rv_[j] = 0.0;
-            for(int k = j + 1; k <= i; ++k) {
-              h = h_L_[ idxmap(k, j, num_vecs_ + 1)];
-              h_rv_[j] += H[ idxmap(i, k, num_vecs_ + 1) ] * h;
-            }
-          }
-
-          // and do one more tri solve with L^T: h_aux = (I-L)^{-1}h_rv
-          for(int j = 0; j <= i; ++j) {
-            h_aux_[j] = h_rv_[j];
-            s = 0.0;
-            for(int k = 0; k < j; ++k) {
-              s += h_L_[ idxmap(j, k, num_vecs_ + 1) ] * h_aux_[k];
-            } // for k
-            h_aux_[j] -= s;
-          }   // for j
-
-          // and now subtract that from h_H
-          for(int j=0; j<=i; ++j) {
-            H[ idxmap(i, j, num_vecs_ + 1) ] -= h_aux_[j];
-          }
-
-          vec_Hcolumn_->setCurrentSize(i + 1);
-          vec_Hcolumn_->update(&H[ idxmap(i, 0, num_vecs_ + 1)], memory::HOST, memory::DEVICE); 
-
-          vector_handler_->massAxpy(n, vec_Hcolumn_, i, V,  vec_w_, memspace);
-          // normalize (second synch)
-          t = vector_handler_->dot(vec_w_, vec_w_, memspace);  
-          //set the last entry in Hessenberg matrix
-          t = sqrt(t);
-          H[ idxmap(i, i + 1, num_vecs_ + 1) ] = t;    
-          if(fabs(t) > EPSILON) {
-            t = 1.0 / t;
-            vector_handler_->scal(&t, vec_w_, memspace);  
-          } else {
-            assert(0 && "Iterative refinement failed, Krylov vector with ZERO norm\n");
-            return -1;
-          }
-          return 0;
-          break;
-
-        default:
-          assert(0 && "Iterative refinement failed, wrong orthogonalization.\n");
-          return -1;
-          break;
-
-      }//switch
-    } else {
-      out::error() << "Not implemented (yet)" << std::endl;
-      return -1;
+    if (memspace != memory::DEVICE) {
+      out::error() << "Support for Gram-Scmidt on CPU not implemented!\n";
+      return 1;
     }
+
+    double t;
+    double s;
+
+    switch (variant_) {
+      case mgs: 
+        vec_w_->setData(V->getVectorData(i + 1, memspace), memspace);
+        for(int j = 0; j <= i; ++j) {
+          t = 0.0;
+          vec_v_->setData( V->getVectorData(j, memspace), memspace);
+          t = vector_handler_->dot(vec_v_, vec_w_, memspace);  
+          H[ idxmap(i, j, num_vecs_ + 1) ] = t; 
+          t *= -1.0;
+          vector_handler_->axpy(&t, vec_v_, vec_w_, memspace);  
+        }
+        t = 0.0;
+        t = vector_handler_->dot(vec_w_, vec_w_, memspace);
+        //set the last entry in Hessenberg matrix
+        t = sqrt(t);
+        H[ idxmap(i, i + 1, num_vecs_ + 1) ] = t; 
+        if(fabs(t) > EPSILON) {
+          t = 1.0/t;
+          vector_handler_->scal(&t, vec_w_, memspace);  
+        } else {
+          assert(0 && "Gram-Schmidt failed, vector with ZERO norm\n");
+          return -1;
+        }
+        break;
+
+      case cgs2:
+        vec_v_->setData(V->getVectorData(i + 1, memspace), memspace);
+        vector_handler_->gemv('T', n, i + 1, &ONE, &ZERO, V,  vec_v_, vec_Hcolumn_, memspace);
+        // V(:,i+1) = V(:, i+1) -  V(:,1:i)*Hcol
+        vector_handler_->gemv('N', n, i + 1, &ONE, &MINUSONE, V, vec_Hcolumn_, vec_v_, memspace );  
+        mem_.deviceSynchronize();
+        
+        // copy H_col to aux, we will need it later
+        vec_Hcolumn_->setDataUpdated(memspace);
+        vec_Hcolumn_->setCurrentSize(i + 1);
+        vec_Hcolumn_->deepCopyVectorData(h_aux_, 0, memory::HOST);
+        mem_.deviceSynchronize();
+
+        //Hcol = V(:,1:i)^T*V(:,i+1);
+        vector_handler_->gemv('T', n, i + 1, &ONE, &ZERO, V,  vec_v_, vec_Hcolumn_, memspace);
+        mem_.deviceSynchronize();
+
+        // V(:,i+1) = V(:, i+1) -  V(:,1:i)*Hcol
+        vector_handler_->gemv('N', n, i + 1, &ONE, &MINUSONE, V, vec_Hcolumn_, vec_v_, memspace );  
+        mem_.deviceSynchronize();
+
+        // copy H_col to H
+        vec_Hcolumn_->setDataUpdated(memspace);
+        vec_Hcolumn_->deepCopyVectorData(&H[ idxmap(i, 0, num_vecs_ + 1)], 0, memory::HOST);
+        mem_.deviceSynchronize();
+
+        // add both pieces together (unstable otherwise, careful here!!)
+        t = 0.0;
+        for(int j = 0; j <= i; ++j) {
+          H[ idxmap(i, j, num_vecs_ + 1)] += h_aux_[j];
+        }
+
+        t = vector_handler_->dot(vec_v_, vec_v_, memspace);  
+        //set the last entry in Hessenberg matrix
+        t = sqrt(t);
+        H[ idxmap(i, i + 1, num_vecs_ + 1) ] = t; 
+
+        if(fabs(t) > EPSILON) {
+          t = 1.0/t;
+          vector_handler_->scal(&t, vec_v_, memspace);  
+        } else {
+          assert(0 && "Gram-Schmidt failed, vector with ZERO norm\n");
+          return -1;
+        }
+        return 0;
+        break;
+
+      case mgs_two_synch:
+        // V[1:i]^T[V[i] w]
+        vec_v_->setData(V->getVectorData(i, memspace), memspace);
+        vec_w_->setData(V->getVectorData(i + 1, memspace), memspace);
+        vec_rv_->setCurrentSize(i + 1);
+
+        vector_handler_->massDot2Vec(n, V, i, vec_v_, vec_rv_, memspace);
+        vec_rv_->setDataUpdated(memspace);
+        vec_rv_->copyData(memspace, memory::HOST);
+
+        vec_rv_->deepCopyVectorData(&h_L_[idxmap(i, 0, num_vecs_ + 1)], 0, memory::HOST);
+        h_rv_ = vec_rv_->getVectorData(1, memory::HOST);
+
+        for(int j=0; j<=i; ++j) {
+          H[ idxmap(i, j, num_vecs_ + 1) ] = 0.0;
+        }
+        // triangular solve
+        for(int j = 0; j <= i; ++j) {
+          H[ idxmap(i, j, num_vecs_ + 1) ] = h_rv_[j];
+          s = 0.0;
+          for(int k = 0; k < j; ++k) {
+            s += h_L_[ idxmap(j, k, num_vecs_ + 1) ] * H[ idxmap(i, k, num_vecs_ + 1) ];
+          } // for k
+          H[ idxmap(i, j, num_vecs_ + 1) ] -= s; 
+        }   // for j
+        vec_Hcolumn_->setCurrentSize(i + 1);
+        vec_Hcolumn_->update(&H[ idxmap(i, 0, num_vecs_ + 1)], memory::HOST, memspace); 
+        vector_handler_->massAxpy(n, vec_Hcolumn_, i, V, vec_w_, memspace);
+
+        // normalize (second synch)
+        t = vector_handler_->dot(vec_w_, vec_w_, memspace);  
+        //set the last entry in Hessenberg matrix
+        t = sqrt(t);
+        H[ idxmap(i, i + 1, num_vecs_ + 1)] = t;    
+        if(fabs(t) > EPSILON) {
+          t = 1.0 / t;
+          vector_handler_->scal(&t, vec_w_, memspace);  
+        } else {
+          assert(0 && "Iterative refinement failed, Krylov vector with ZERO norm\n");
+          return -1;
+        }
+        return 0;
+        break;
+
+      case mgs_pm:
+        vec_v_->setData(V->getVectorData(i, memspace), memspace);
+        vec_w_->setData(V->getVectorData(i + 1, memspace), memspace);
+        vec_rv_->setCurrentSize(i + 1);
+
+        vector_handler_->massDot2Vec(n, V, i, vec_v_, vec_rv_, memspace);
+        vec_rv_->setDataUpdated(memspace);
+        vec_rv_->copyData(memspace, memory::HOST);
+
+        vec_rv_->deepCopyVectorData(&h_L_[idxmap(i, 0, num_vecs_ + 1)], 0, memory::HOST);
+        h_rv_ = vec_rv_->getVectorData(1, memory::HOST);
+
+        for(int j = 0; j <= i; ++j) {
+          H[ idxmap(i, j, num_vecs_ + 1) ] = 0.0;
+        }
+
+        //triangular solve
+        for(int j = 0; j <= i; ++j) {
+          H[ idxmap(i, j, num_vecs_ + 1) ] = h_rv_[j];
+          s = 0.0;
+          for(int k = 0; k < j; ++k) {
+            s += h_L_[ idxmap(j, k, num_vecs_ + 1) ] * H[ idxmap(i, k, num_vecs_ + 1) ];
+          } // for k
+          H[ idxmap(i, j, num_vecs_ + 1) ] -= s;
+        }   // for j
+
+        // now compute h_rv = L^T h_H
+        double h;
+        for(int j = 0; j <= i; ++j) {
+          // go through COLUMN OF L
+          h_rv_[j] = 0.0;
+          for(int k = j + 1; k <= i; ++k) {
+            h = h_L_[ idxmap(k, j, num_vecs_ + 1)];
+            h_rv_[j] += H[ idxmap(i, k, num_vecs_ + 1) ] * h;
+          }
+        }
+
+        // and do one more tri solve with L^T: h_aux = (I-L)^{-1}h_rv
+        for(int j = 0; j <= i; ++j) {
+          h_aux_[j] = h_rv_[j];
+          s = 0.0;
+          for(int k = 0; k < j; ++k) {
+            s += h_L_[ idxmap(j, k, num_vecs_ + 1) ] * h_aux_[k];
+          } // for k
+          h_aux_[j] -= s;
+        }   // for j
+
+        // and now subtract that from h_H
+        for(int j=0; j<=i; ++j) {
+          H[ idxmap(i, j, num_vecs_ + 1) ] -= h_aux_[j];
+        }
+
+        vec_Hcolumn_->setCurrentSize(i + 1);
+        vec_Hcolumn_->update(&H[ idxmap(i, 0, num_vecs_ + 1)], memory::HOST, memspace); 
+
+        vector_handler_->massAxpy(n, vec_Hcolumn_, i, V,  vec_w_, memspace);
+        // normalize (second synch)
+        t = vector_handler_->dot(vec_w_, vec_w_, memspace);  
+        //set the last entry in Hessenberg matrix
+        t = sqrt(t);
+        H[ idxmap(i, i + 1, num_vecs_ + 1) ] = t;    
+        if(fabs(t) > EPSILON) {
+          t = 1.0 / t;
+          vector_handler_->scal(&t, vec_w_, memspace);  
+        } else {
+          assert(0 && "Iterative refinement failed, Krylov vector with ZERO norm\n");
+          return -1;
+        }
+        return 0;
+        break;
+
+      default:
+        assert(0 && "Iterative refinement failed, wrong orthogonalization.\n");
+        return -1;
+        break;
+    } //switch
+
     return 0;
-  }//orthogonalize
-}
+  } // int orthogonalize()
+
+} // namespace ReSolve
--- resolve/LinSolverIterativeFGMRES.cpp
+++ resolve/LinSolverIterativeFGMRES.cpp
@@ -11,6 +11,11 @@ namespace ReSolve
 {
   using out = io::Logger;
 
+  /**
+   * @brief Construct a new (F)GMRES solver
+   * 
+   * @param memspace 
+   */
   LinSolverIterativeFGMRES::LinSolverIterativeFGMRES(std::string memspace)
   {
     if (memspace == "cpu") {
@@ -99,14 +104,14 @@ namespace ReSolve
     n_ = A_->getNumRows();
 
     d_V_ = new vector_type(n_, restart_ + 1);
-    d_V_->allocate(memory::DEVICE);      
+    d_V_->allocate(memspace_);      
     if (flexible_) {
       d_Z_ = new vector_type(n_, restart_ + 1);
     } else {
       // otherwise Z is just a one vector, not multivector and we dont keep it
       d_Z_ = new vector_type(n_);
     }
-    d_Z_->allocate(memory::DEVICE);      
+    d_Z_->allocate(memspace_);      
     h_H_  = new real_type[restart_ * (restart_ + 1)];
     h_c_  = new real_type[restart_];      // needed for givens
     h_s_  = new real_type[restart_];      // same
@@ -138,10 +143,10 @@ namespace ReSolve
     vector_type* vec_z = new vector_type(n_);
     //V[0] = b-A*x_0
     //debug
-    d_Z_->setToZero(memory::DEVICE);
-    d_V_->setToZero(memory::DEVICE);
+    d_Z_->setToZero(memspace_);
+    d_V_->setToZero(memspace_);
 
-    rhs->deepCopyVectorData(d_V_->getData(memory::DEVICE), 0, memory::DEVICE);  
+    rhs->deepCopyVectorData(d_V_->getData(memspace_), 0, memspace_);  
     matrix_handler_->matvec(A_, x, d_V_, &MINUSONE, &ONE, "csr", memspace_); 
     rnorm = 0.0;
     bnorm = vector_handler_->dot(rhs, rhs, memspace_);
@@ -194,18 +199,18 @@ namespace ReSolve
         it++;
 
         // Z_i = (LU)^{-1}*V_i
-        vec_v->setData( d_V_->getVectorData(i, memory::DEVICE), memory::DEVICE);
+        vec_v->setData( d_V_->getVectorData(i, memspace_), memspace_);
         if (flexible_) {
-          vec_z->setData( d_Z_->getVectorData(i, memory::DEVICE), memory::DEVICE);
+          vec_z->setData( d_Z_->getVectorData(i, memspace_), memspace_);
         } else {
-          vec_z->setData( d_Z_->getVectorData(0, memory::DEVICE), memory::DEVICE);
+          vec_z->setData( d_Z_->getVectorData(0, memspace_), memspace_);
         }
         this->precV(vec_v, vec_z);
         mem_.deviceSynchronize();
 
         // V_{i+1}=A*Z_i
 
-        vec_v->setData( d_V_->getVectorData(i + 1, memory::DEVICE), memory::DEVICE);
+        vec_v->setData( d_V_->getVectorData(i + 1, memspace_), memspace_);
 
         matrix_handler_->matvec(A_, vec_z, vec_v, &ONE, &ZERO,"csr", memspace_); 
 
@@ -266,19 +271,19 @@ namespace ReSolve
       // get solution
       if (flexible_) {
         for(j = 0; j <= i; j++) {
-          vec_z->setData( d_Z_->getVectorData(j, memory::DEVICE), memory::DEVICE);
+          vec_z->setData( d_Z_->getVectorData(j, memspace_), memspace_);
           vector_handler_->axpy(&h_rs_[j], vec_z, x, memspace_);
         }
       } else {
-        mem_.setZeroArrayOnDevice(d_Z_->getData(memory::DEVICE), d_Z_->getSize());
-        vec_z->setData( d_Z_->getVectorData(0, memory::DEVICE), memory::DEVICE);
+        mem_.setZeroArrayOnDevice(d_Z_->getData(memspace_), d_Z_->getSize());
+        vec_z->setData( d_Z_->getVectorData(0, memspace_), memspace_);
         for(j = 0; j <= i; j++) {
-          vec_v->setData( d_V_->getVectorData(j, memory::DEVICE), memory::DEVICE);
+          vec_v->setData( d_V_->getVectorData(j, memspace_), memspace_);
           vector_handler_->axpy(&h_rs_[j], vec_v, vec_z, memspace_);
         }
         // now multiply d_Z by precon
 
-        vec_v->setData( d_V_->getData(memory::DEVICE), memory::DEVICE);
+        vec_v->setData( d_V_->getData(memspace_), memspace_);
         this->precV(vec_z, vec_v);
         // and add to x 
         vector_handler_->axpy(&ONE, vec_v, x, memspace_);
@@ -291,7 +296,7 @@ namespace ReSolve
         outer_flag = 0;
       }
 
-      rhs->deepCopyVectorData(d_V_->getData(memory::DEVICE), 0, memory::DEVICE);  
+      rhs->deepCopyVectorData(d_V_->getData(memspace_), 0, memspace_);  
       matrix_handler_->matvec(A_, x, d_V_, &MINUSONE, &ONE,"csr", memspace_); 
       rnorm = vector_handler_->dot(d_V_, d_V_, memspace_);
       // rnorm = ||V_1||
@@ -332,7 +337,6 @@ namespace ReSolve
   void  LinSolverIterativeFGMRES::precV(vector_type* rhs, vector_type* x)
   { 
     LU_solver_->solve(rhs, x);
-    //  x->update(rhs->getData(memory::DEVICE), memory::DEVICE, memory::DEVICE);
   }
 
 }//namespace
