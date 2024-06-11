6e4d7de0149260d1ea6ce66eecf987ee4d8955c0
Slaven Peles <peless@ornl.gov>
2024-06-11 16:52:58 -0400

    Fix flipped columns/rows in CSC matrix implementation.

--- resolve/matrix/Csc.hpp
+++ resolve/matrix/Csc.hpp
@@ -9,18 +9,27 @@ namespace ReSolve { namespace matrix {
       Csc();
       Csc(index_type n, index_type m, index_type nnz);
       Csc(index_type n, 
-                index_type m, 
-                index_type nnz,
-                bool symmetric,
-                bool expanded);
+          index_type m, 
+          index_type nnz,
+          bool symmetric,
+          bool expanded);
       ~Csc();
 
       virtual index_type* getRowData(memory::MemorySpace memspace);
       virtual index_type* getColData(memory::MemorySpace memspace);
       virtual real_type*  getValues( memory::MemorySpace memspace); 
 
-      virtual int updateData(index_type* row_data, index_type* col_data, real_type* val_data, memory::MemorySpace memspaceIn, memory::MemorySpace memspaceOut); 
-      virtual int updateData(index_type* row_data, index_type* col_data, real_type* val_data, index_type new_nnz, memory::MemorySpace memspaceIn, memory::MemorySpace memspaceOut); 
+      virtual int updateData(index_type* row_data,
+                             index_type* col_data,
+                             real_type* val_data,
+                             memory::MemorySpace memspaceIn,
+                             memory::MemorySpace memspaceOut); 
+      virtual int updateData(index_type* row_data,
+                             index_type* col_data,
+                             real_type* val_data,
+                             index_type new_nnz,
+                             memory::MemorySpace memspaceIn,
+                             memory::MemorySpace memspaceOut); 
 
       virtual int allocateMatrixData(memory::MemorySpace memspace);
 
