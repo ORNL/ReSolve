7d82835367c5e48190b62fe4874ab2ed2f8e5368
Slaven Peles <peless@ornl.gov>
2024-10-09 20:20:00 -0400

    matrix getters don't sync.

--- tests/functionality/testKLU_RocSolver_FGMRES.cpp
+++ tests/functionality/testKLU_RocSolver_FGMRES.cpp
@@ -77,6 +77,7 @@ int main(int argc, char *argv[])
     return -1;
   }
   ReSolve::matrix::Csr* A = ReSolve::io::createCsrFromFile(mat1);
+  A->copyData(ReSolve::memory::DEVICE);
   mat1.close();
 
   // Read first rhs vector
@@ -193,6 +194,7 @@ int main(int argc, char *argv[])
     return -1;
   }
   ReSolve::io::updateMatrixFromFile(mat2, A);
+  A->copyData(ReSolve::memory::DEVICE);
   mat2.close();
 
   // Load the second rhs vector
