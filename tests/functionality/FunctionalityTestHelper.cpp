66096762432ed3050320ad6863e5a17023b28ea3
Harry Hughes <harrnh3@gmail.com>
2024-10-16 11:09:15 -0400

    initial migration of header to cpp

---
+++ tests/functionality/FunctionalityTestHelper.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+#include <resolve/vector/Vector.hpp>
+#include <resolve/matrix/io.hpp>
+#include <resolve/matrix/Coo.hpp>
+#include <resolve/matrix/Csr.hpp>
+#include <resolve/matrix/Csc.hpp>
+#include <resolve/matrix/MatrixHandler.hpp>
+#include <resolve/vector/VectorHandler.hpp>
+#include <resolve/LinSolverDirectKLU.hpp>
+#include <resolve/LinSolverIterativeFGMRES.hpp>
+#include <resolve/workspace/LinAlgWorkspace.hpp>
+#include <resolve/SystemSolver.hpp>
+
+#if defined (RESOLVE_USE_CUDA)
+#include <resolve/LinSolverDirectCuSolverRf.hpp>
+  using workspace_type = ReSolve::LinAlgWorkspaceCUDA;
+  std::string memory_space("cuda");
+#elif defined (RESOLVE_USE_HIP)
+#include <resolve/LinSolverDirectRocSolverRf.hpp>
+  using workspace_type = ReSolve::LinAlgWorkspaceHIP;
+  std::string memory_space("hip");
+#else
+  using workspace_type = ReSolve::LinAlgWorkspaceCpu;
+  std::string memory_space("cpu");
+#endif
+
+#include "FunctionalityTestHelper.hpp"
