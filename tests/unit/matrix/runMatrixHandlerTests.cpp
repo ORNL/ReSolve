12e4bd1026be46f9e50c5a27c78269bc21555843
shakedregev <shakedvregev@gmail.com>
2025-02-12 22:35:45 +0000

    added workspace template

--- tests/unit/matrix/runMatrixHandlerTests.cpp
+++ tests/unit/matrix/runMatrixHandlerTests.cpp
@@ -5,34 +5,37 @@
 #include <resolve/matrix/io.hpp>
 #include "MatrixHandlerTests.hpp"
 
-#if defined (RESOLVE_USE_CUDA)
-  ReSolve::LinAlgWorkspaceCUDA workspace;
-#elif defined (RESOLVE_USE_HIP)
-  ReSolve::LinAlgWorkspaceHIP workspace;
-#else
-  ReSolve::LinAlgWorkspaceCpu workspace;
+template<typename WorkspaceType>
+void runTests(const std::string& backendName, ReSolve::tests::TestingResults& result) {
+    std::cout << "Running tests with " << backendName << ":\n";
+    
+    WorkspaceType workspace;
+    workspace.initializeHandles();
+    ReSolve::MatrixHandler handler(&workspace);
+
+    ReSolve::tests::MatrixHandlerTests test(handler);
+    result += test.matrixHandlerConstructor();
+    result += test.matrixInfNorm(1000000);
+    result += test.matVec(50);
+    result += test.csc2csr(1024, 1024);
+    result += test.csc2csr(1024, 2048);
+    result += test.csc2csr(2048, 1024);
+    std::cout << "\n";
+}
+
+int main(int, char**) {
+    ReSolve::tests::TestingResults result;
+
+    runTests<ReSolve::LinAlgWorkspaceCpu>("CPU", result);
+
+#ifdef RESOLVE_USE_CUDA
+    runTests<ReSolve::LinAlgWorkspaceCUDA>("CUDA backend", result);
 #endif
 
-int main(int, char**)
-{
-  #if defined (RESOLVE_USE_CUDA)
-    std::cout << "Running tests with CUDA backend:\n";
-  #elif defined (RESOLVE_USE_HIP)
-    std::cout << "Running tests with HIP backend:\n";
-  #else
-    std::cout << "Running tests on CPU:\n";
-  #endif
-  ReSolve::tests::TestingResults result; 
-  workspace.initializeHandles();
-  ReSolve::MatrixHandler handler(&workspace);
-  ReSolve::tests::MatrixHandlerTests test(handler);
-  result += test.matrixHandlerConstructor();
-  result += test.matrixInfNorm(1000000);
-  result += test.matVec(50);
-  result += test.csc2csr(1024, 1024);
-  result += test.csc2csr(1024, 2048);
-  result += test.csc2csr(2048, 1024);
-  std::cout << "\n";
-  return result.summary();
+#ifdef RESOLVE_USE_HIP
+    runTests<ReSolve::LinAlgWorkspaceHIP>("HIP backend", result);
+#endif
+
+    return result.summary();
 }
 
