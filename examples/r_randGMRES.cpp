cb6bd7d1e68f53cc3f8d0163b0b01e040ff93e9f
Slaven Peles <peless@ornl.gov>
2025-02-13 17:26:37 -0500

    Create GMRES example objects on stack.

--- examples/r_randGMRES.cpp
+++ examples/r_randGMRES.cpp
@@ -33,7 +33,6 @@ int example(int argc, char *argv[])
   using namespace ReSolve;
   using namespace ReSolve::constants;
   using namespace ReSolve::examples;
-  using real_type  = ReSolve::real_type;
   using vector_type = ReSolve::vector::Vector;
 
   (void) argc; // TODO: Check if the number of input parameters is correct.
@@ -41,26 +40,26 @@ int example(int argc, char *argv[])
   std::string  rhsFileName = argv[2];
 
 
-  workspace_type* workspace = new workspace_type();
-  workspace->initializeHandles();
+  workspace_type workspace;
+  workspace.initializeHandles();
 
   // Create a helper object (computing errors, printing summaries, etc.)
-  ExampleHelper<workspace_type> helper(*workspace);
+  ExampleHelper<workspace_type> helper(workspace);
 
-  MatrixHandler* matrix_handler =  new MatrixHandler(workspace);
-  VectorHandler* vector_handler =  new VectorHandler(workspace);
-  real_type* rhs = nullptr;
-  real_type* x   = nullptr;
+  MatrixHandler matrix_handler(&workspace);
+  VectorHandler vector_handler(&workspace);
 
   matrix::Csr* A = nullptr;
   vector_type* vec_rhs = nullptr;
   vector_type* vec_x   = nullptr;
-  // vector_type* vec_r   = nullptr;
 
-  GramSchmidt* GS = new GramSchmidt(vector_handler, GramSchmidt::CGS2);
+  GramSchmidt GS(&vector_handler, GramSchmidt::CGS2);
 
-  LinSolverDirectRocSparseILU0* Rf = new LinSolverDirectRocSparseILU0(workspace);
-  LinSolverIterativeRandFGMRES* FGMRES = new LinSolverIterativeRandFGMRES(matrix_handler, vector_handler,LinSolverIterativeRandFGMRES::cs, GS);
+  LinSolverDirectRocSparseILU0 Precond(&workspace);
+  LinSolverIterativeRandFGMRES FGMRES(&matrix_handler,
+                                      &vector_handler,
+                                      LinSolverIterativeRandFGMRES::cs,
+                                      &GS);
 
   std::ifstream mat_file(matrix_filename);
   if(!mat_file.is_open())
@@ -76,57 +75,39 @@ int example(int argc, char *argv[])
   }
   bool is_expand_symmetric = true;
   A = io::createCsrFromFile(mat_file, is_expand_symmetric);
-
-  rhs = io::createArrayFromFile(rhs_file);
-  x = new real_type[A->getNumRows()];
-  vec_rhs = new vector_type(A->getNumRows());
-  vec_x = new vector_type(A->getNumRows());
-  vec_x->allocate(memory::HOST);
-  //iinit guess is 0U
-  vec_x->allocate(memory::DEVICE);
-  vec_x->setToZero(memory::DEVICE);
-  // vec_r = new vector_type(A->getNumRows());
-
+  vec_rhs = io::createVectorFromFile(rhs_file);
   mat_file.close();
   rhs_file.close();
 
+  vec_x = new vector_type(A->getNumRows());
+  vec_x->allocate(memory::DEVICE);
   A->syncData(memory::DEVICE);
-  vec_rhs->copyDataFrom(rhs, memory::HOST, memory::DEVICE);
+  vec_rhs->syncData(memory::DEVICE);
 
   printSystemInfo(matrix_filename, A);
 
-  //Now call the solver
-  // real_type norm_b;
-  matrix_handler->setValuesChanged(true, memory::DEVICE);
+  matrix_handler.setValuesChanged(true, memory::DEVICE);
 
-  Rf->setup(A);
-  FGMRES->setRestart(150);
-  FGMRES->setMaxit(2500);
-  FGMRES->setTol(1e-12);
-  FGMRES->setup(A);
-  GS->setup(FGMRES->getKrand(), FGMRES->getRestart()); 
+  Precond.setup(A);
+  FGMRES.setRestart(150);
+  FGMRES.setMaxit(2500);
+  FGMRES.setTol(1e-12);
+  FGMRES.setup(A);
+  GS.setup(FGMRES.getKrand(), FGMRES.getRestart()); 
 
-  //matrix_handler->setValuesChanged(true, memory::DEVICE);
-  FGMRES->resetMatrix(A);
-  FGMRES->setupPreconditioner("LU", Rf);
-  FGMRES->setFlexible(1); 
+  FGMRES.resetMatrix(A);
+  FGMRES.setupPreconditioner("LU", &Precond);
+  FGMRES.setFlexible(1); 
 
-  vec_rhs->copyDataFrom(rhs, memory::HOST, memory::DEVICE);
-  FGMRES->solve(vec_rhs, vec_x);
+  FGMRES.solve(vec_rhs, vec_x);
 
   // Print summary of results
   helper.resetSystem(A, vec_rhs, vec_x);
-  helper.printIrSummary(FGMRES);
+  helper.printIrSummary(&FGMRES);
 
   delete A;
-  delete Rf;
-  delete [] x;
-  delete [] rhs;
   delete vec_rhs;
   delete vec_x;
-  delete workspace;
-  delete matrix_handler;
-  delete vector_handler;
 
   return 0;
 }
