09d73627b279535e918c243b61e7aaf02442da1f
shakedregev <shakedvregev@gmail.com>
2025-02-13 16:22:48 +0000

    rectangular cases working

--- tests/unit/matrix/runMatrixHandlerTests.cpp
+++ tests/unit/matrix/runMatrixHandlerTests.cpp
@@ -18,8 +18,13 @@ void runTests(const std::string& backendName, ReSolve::tests::TestingResults& re
     result += test.matrixInfNorm(1000000);
     result += test.matVec(50);
     result += test.csc2csr(5, 5);
-    // result += test.csc2csr(1024, 2048);
-    // result += test.csc2csr(2048, 1024);
+    result += test.csc2csr(5, 3);
+    result += test.csc2csr(3, 5);
+    result += test.csc2csr(1024, 1024);
+    result += test.csc2csr(1024, 2048);
+    result += test.csc2csr(2048, 1024);
+    result += test.csc2csr(1024, 1200);
+    result += test.csc2csr(1200, 1024);
     std::cout << "\n";
 }
 
