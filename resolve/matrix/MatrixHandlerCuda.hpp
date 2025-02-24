8271ea75321a496de66fbd2c8b1676f167ad6c24
shakedregev <shakedvregev@gmail.com>
2025-02-24 20:20:18 +0000

    added CUDA transpose

--- resolve/matrix/MatrixHandlerCuda.hpp
+++ resolve/matrix/MatrixHandlerCuda.hpp
@@ -4,7 +4,7 @@
 #include <resolve/matrix/MatrixHandlerImpl.hpp>
 
 namespace ReSolve
-{ 
+{
   namespace vector
   {
     class Vector;
@@ -23,27 +23,28 @@ namespace ReSolve
 namespace ReSolve {
   /**
    * @class MatrixHandlerCuda
-   * 
+   *
    * @brief CUDA implementation of the matrix handler.
    */
   class MatrixHandlerCuda : public MatrixHandlerImpl
   {
     using vector_type = vector::Vector;
-    
+
     public:
       MatrixHandlerCuda(LinAlgWorkspaceCUDA* workspace);
       virtual ~MatrixHandlerCuda();
 
       int csc2csr(matrix::Csc* A_csc, matrix::Csr* A_csr);
+      int transpose(matrix::Csr* A, matrix::Csr* At);
       virtual int matvec(matrix::Sparse* A,
                  vector_type* vec_x,
                  vector_type* vec_result,
                  const real_type* alpha,
                  const real_type* beta);
       virtual int matrixInfNorm(matrix::Sparse* A, real_type* norm);
-      void setValuesChanged(bool isValuesChanged); 
-    
-    private: 
+      void setValuesChanged(bool isValuesChanged);
+
+    private:
       LinAlgWorkspaceCUDA* workspace_{nullptr};
       bool values_changed_{true}; ///< needed for matvec
 
