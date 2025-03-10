812c759828993fd5e30f67579ba35cc9dee623e8
Shaked Regev <shakedvregev@gmail.comregev@gmail.com>
2025-03-10 16:01:15 -0400

    reuse transpose working in HIP

--- resolve/matrix/MatrixHandlerHip.hpp
+++ resolve/matrix/MatrixHandlerHip.hpp
@@ -37,8 +37,9 @@ namespace ReSolve {
 
       int csc2csr(matrix::Csc* A_csc, matrix::Csr* A_csr);
 
-      int transpose(matrix::Csr* A, matrix::Csr* At);
-      
+      int transpose(matrix::Csr* A, matrix::Csr* At, bool allocated = false) override;
+
+      int addConstantToNonzeroValues(matrix::Sparse* A, real_type alpha);
       virtual int matvec(matrix::Sparse* A,
                          vector_type* vec_x,
                          vector_type* vec_result,
