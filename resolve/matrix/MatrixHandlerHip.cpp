4d7104682a6702dc7f2d303736882e6415b2fb4f
shakedregev <35384901+shakedregev@users.noreply.github.com>
2025-03-14 09:31:43 -0400

    Update resolve/matrix/MatrixHandlerHip.cpp
    Co-authored-by: pelesh <peless@ornl.gov>

--- resolve/matrix/MatrixHandlerHip.cpp
+++ resolve/matrix/MatrixHandlerHip.cpp
@@ -243,6 +243,8 @@ namespace ReSolve {
    * @param[in]  A - Sparse matrix
    * @param[out] At - Transposed matrix
    * @return int error_sum, 0 if successful
+   *
+   * @warning This method works only for `real_type == double`.
    */
   int MatrixHandlerHip::transpose(matrix::Csr* A, matrix::Csr* At)
   {
