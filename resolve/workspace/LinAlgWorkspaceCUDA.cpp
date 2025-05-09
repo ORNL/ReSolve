a3b81fcde3f28502f83c74ea203e3e97450f1730
shakedregev <35384901+shakedregev@users.noreply.github.com>
2025-05-09 13:56:53 -0400

    Update resolve/workspace/LinAlgWorkspaceCUDA.cpp
    Co-authored-by: pelesh <peless@ornl.gov>

--- resolve/workspace/LinAlgWorkspaceCUDA.cpp
+++ resolve/workspace/LinAlgWorkspaceCUDA.cpp
@@ -22,7 +22,6 @@ namespace ReSolve
     if (buffer_spmv_ != nullptr)  mem_.deleteOnDevice(buffer_spmv_);
     if (d_r_size_ != 0)  mem_.deleteOnDevice(d_r_);
     if (norm_buffer_ready_) mem_.deleteOnDevice(buffer_1norm_);
-    if (transpose_workspace_ != nullptr) mem_.deleteOnDevice(transpose_workspace_);
     cusparseDestroy(handle_cusparse_);
     cusolverSpDestroy(handle_cusolversp_);
     cublasDestroy(handle_cublas_);
