22c2a411b8c27cd41beaaa6e42cbb9f81e98be31
Slaven Peles <peless@ornl.gov>
2025-05-10 01:35:40 -0400

    Add vector constructor test.

--- tests/unit/vector/runVectorTests.cpp
+++ tests/unit/vector/runVectorTests.cpp
@@ -7,16 +7,18 @@ int main(int, char**)
 {
   ReSolve::tests::TestingResults result;
 
-  ReSolve::tests::VectorTests tests();
+  ReSolve::tests::VectorTests test;
 
   {
     std::cout << "Running tests on CPU:\n";
+    result += test.vectorConstructor();
 
   }
 
 #ifdef RESOLVE_USE_GPU
   {
     std::cout << "Running tests on GPU:\n";
+    result += test.vectorConstructor();
 
   }
 #endif
