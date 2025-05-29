56e727fb646eb7b0e81a6b136e2a45edf32ad088
Adham Ibrahim <37982706+adham-ibrahim7@users.noreply.github.com>
2025-05-29 08:36:58 -0400

    Update tests/unit/vector/VectorTests.hpp
    Co-authored-by: shakedregev <35384901+shakedregev@users.noreply.github.com>

--- tests/unit/vector/VectorTests.hpp
+++ tests/unit/vector/VectorTests.hpp
@@ -100,32 +100,6 @@ namespace ReSolve {
 
           return status.report(__func__);
         }
-
-      private:
-        ReSolve::VectorHandler& handler_;
-        ReSolve::memory::MemorySpace memspace_{memory::HOST};
-
-        // we can verify through norm but that would defeat the purpose of testing vector handler ...
-        bool verifyAnswer(vector::Vector& x, real_type answer)
-        {
-          bool status = true;
-
-          if (memspace_ == memory::DEVICE) {
-            x.syncData(memory::HOST);
-          }
-
-          for (index_type i = 0; i < x.getSize(); ++i) {
-            // std::cout << x->getData("cpu")[i] << "\n";
-            if (!isEqual(x.getData(memory::HOST)[i], answer)) {
-              std::cout << std::setprecision(16);
-              status = false;
-              std::cout << "Solution vector element x[" << i << "] = " << x.getData(memory::HOST)[i]
-                << ", expected: " << answer << "\n";
-              break; 
-            }
-          }
-          return status;
-        }
     };//class
   } // namespace tests
 } //namespace ReSolve
