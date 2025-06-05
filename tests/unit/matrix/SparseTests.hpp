0c49232dd7920bcaccf4d72e136109197c139afe
shakedregev <shakedvregev@gmail.com>
2025-06-05 20:28:14 +0000

    initialized h_val_data

--- tests/unit/matrix/SparseTests.hpp
+++ tests/unit/matrix/SparseTests.hpp
@@ -16,7 +16,7 @@ namespace ReSolve { namespace tests {
   public:
     SparseTests(memory::MemorySpace memspace = memory::HOST) : memspace_(memspace)
     {
-      
+
     }
 
     ~SparseTests()
@@ -25,24 +25,24 @@ namespace ReSolve { namespace tests {
 
     /**
      * @brief Constructor test for sparse matrix
-     * 
+     *
      * Constructs a CSR matrix with the given parameters and confirms that the
      * parameters are stored correctly.
-     * 
+     *
      * @param[in] n - number of rows
      * @param[in] m - number of columns
      * @param[in] row_density - number of non-zeros per row
      * @param[in] is_symmetric - true if the matrix is symmetric, false otherwise
-     * @param[in] is_expanded - true if the matrix is expanded (all non-zeros 
+     * @param[in] is_expanded - true if the matrix is expanded (all non-zeros
      are stored explicitly, not assumed based on symmetry), false otherwise
-     * 
+     *
      * @return TestOutcome indicating success or failure of the test
      */
-    TestOutcome constructor(index_type n, 
-                            index_type m, 
+    TestOutcome constructor(index_type n,
+                            index_type m,
                             index_type nnz,
                             bool is_symmetric = false,
-                            bool is_expanded = true) 
+                            bool is_expanded = true)
     {
       TestStatus success;
       success = true;
@@ -59,14 +59,14 @@ namespace ReSolve { namespace tests {
 
     /**
      * @brief Test set data pointers for the sparse matrix
-     * 
+     *
      * Sets the row, column, and value data pointers for the sparse matrix and
      * checks if the pointers are set correctly.
-     * 
+     *
      * @param[in] n - number of rows
      * @param[in] m - number of columns
      * @param[in] row_density - number of non-zeros per row
-     * 
+     *
      * @return TestOutcome indicating success or failure of the test
      */
     TestOutcome setDataPointers(index_type n, index_type m, index_type row_density)
@@ -94,8 +94,8 @@ namespace ReSolve { namespace tests {
       if (A.setDataPointers(h_row_data, h_col_data, h_val_data, memory::HOST) != 0) {
         std::cout << "Failed to set data pointers.\n";
         success = false;
-      } else if (A.getRowData(memory::HOST) != h_row_data || 
-                A.getColData(memory::HOST) != h_col_data || 
+      } else if (A.getRowData(memory::HOST) != h_row_data ||
+                A.getColData(memory::HOST) != h_col_data ||
                 A.getValues(memory::HOST) != h_val_data) {
         std::cout << "Data pointers do not point to expected values.\n";
         success = false;
@@ -106,14 +106,14 @@ namespace ReSolve { namespace tests {
 
     /**
      * @brief Test setting values pointer for the sparse matrix
-     * 
+     *
      * Sets the values pointer for the sparse matrix and checks if the pointer
      * points to the expected values.
-     * 
+     *
      * @param[in] n - number of rows
      * @param[in] m - number of columns
      * @param[in] row_density - number of non-zeros per row
-     * 
+     *
      * @return TestOutcome indicating success or failure of the test
      */
     TestOutcome setValuesPointer(index_type n, index_type m, index_type row_density)
@@ -143,15 +143,15 @@ namespace ReSolve { namespace tests {
 
     /**
      * @brief Test copying values into the sparse matrix
-     * 
+     *
      * Copies values into the sparse matrix, modifies the original values,
-     * and verifies the correct unmodified values are stored in the matrix, 
+     * and verifies the correct unmodified values are stored in the matrix,
      * and that the values pointer does not point to the original array.
-     * 
+     *
      * @param[in] n - number of rows
      * @param[in] m - number of columns
      * @param[in] row_density - number of non-zeros per row
-     * 
+     *
      * @return TestOutcome indicating success or failure of the test
      */
     TestOutcome copyValues(index_type n, index_type m, index_type row_density)
@@ -167,7 +167,7 @@ namespace ReSolve { namespace tests {
       for (index_type i = 0; i < nnz; ++i) {
         val_data[i] = static_cast<real_type>(i + 1);
       }
-
+      real_type* h_val_data = nullptr;
       if (A->copyValues(val_data, memory::HOST, memspace_) != 0) {
         std::cout << "Failed to copy values.\n";
         success = false;
@@ -181,12 +181,16 @@ namespace ReSolve { namespace tests {
           std::cout << "Values pointer is null after copy.\n";
           success = false;
         } else {
-          real_type* h_val_data;
           if (memspace_ == memory::HOST) {
             h_val_data = A->getValues(memory::HOST);
           } else {
             real_type* d_val_data = A->getValues(memory::DEVICE);
+            h_val_data = new real_type[nnz];
             mem_.copyArrayDeviceToHost(h_val_data, d_val_data, nnz);
+            if (h_val_data == nullptr) {
+              std::cout << "Failed to copy values from device to host.\n";
+              success = false;
+            }
           }
 
           // Check if the copied values are correct
@@ -213,10 +217,10 @@ namespace ReSolve { namespace tests {
 
     /**
      * @brief Verify that matrix will not reset data pointers when it currently owns data.
-     * 
+     *
      * Copies values into the sparse matrix and then attempts to set the values
      * pointer, which should fail since the matrix owns the data.
-     * 
+     *
      * @param[in] n - number of rows
      * @param[in] m - number of columns
      * @param[in] row_density - number of non-zeros per row
@@ -239,7 +243,7 @@ namespace ReSolve { namespace tests {
       if (A->copyValues(val_data, memory::HOST, memspace_) != 0) {
         std::cout << "Failed to copy values.\n";
         success = false;
-      } 
+      }
 
       index_type* row_data = new index_type[n + 1];
       for (index_type i = 0; i <= n; ++i) {
@@ -261,12 +265,12 @@ namespace ReSolve { namespace tests {
       delete[] col_data;
 
       return success.report(__func__);
-    } 
+    }
 
     /**
      * @brief Verify that matrix will not reset values pointer when it currently owns data.
      *
-     * Copies a data array using `copyValues`, then attempts to set the values pointer, 
+     * Copies a data array using `copyValues`, then attempts to set the values pointer,
      * and verifies that this results in an error.
      *
      * @param[in] n - number of rows
@@ -291,7 +295,7 @@ namespace ReSolve { namespace tests {
       if (A->copyValues(val_data, memory::HOST, memspace_) != 0) {
         std::cout << "Failed to copy values.\n";
         success = false;
-      } 
+      }
 
       if (A->setValuesPointer(val_data, memspace_) == 0) {
         std::cout << "Should not have set values pointer when matrix owns data.\n";
@@ -306,14 +310,14 @@ namespace ReSolve { namespace tests {
 
     /**
      * @brief Test allocating and destroying matrix data
-     * 
+     *
      * Allocates memory for the sparse matrix data and checks if the pointers
      * are not null after allocation. Then destroys the allocated data.
-     * 
+     *
      * @param[in] n - number of rows
      * @param[in] m - number of columns
      * @param[in] row_density - number of non-zeros per row
-     * 
+     *
      * @return TestOutcome indicating success or failure of the test
      */
     TestOutcome allocateAndDestroyData(index_type n, index_type m, index_type row_density)
@@ -328,8 +332,8 @@ namespace ReSolve { namespace tests {
       if (A->allocateMatrixData(memspace_) != 0) {
         std::cout << "Failed to allocate matrix data.\n";
         success = false;
-      } else if (A->getRowData(memspace_) == nullptr || 
-                 A->getColData(memspace_) == nullptr || 
+      } else if (A->getRowData(memspace_) == nullptr ||
+                 A->getColData(memspace_) == nullptr ||
                  A->getValues(memspace_) == nullptr) {
         std::cout << "Matrix data pointers are null after allocation.\n";
         success = false;
@@ -338,8 +342,8 @@ namespace ReSolve { namespace tests {
       if (A->destroyMatrixData(memspace_) != 0) {
         std::cout << "Failed to destroy matrix data.\n";
         success = false;
-      } else if (A->getRowData(memspace_) != nullptr || 
-                 A->getColData(memspace_) != nullptr || 
+      } else if (A->getRowData(memspace_) != nullptr ||
+                 A->getColData(memspace_) != nullptr ||
                  A->getValues(memspace_) != nullptr) {
         std::cout << "Matrix data pointers are not null after destruction.\n";
         success = false;
