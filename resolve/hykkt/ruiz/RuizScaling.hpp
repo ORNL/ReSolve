969e2f8f20172b057923001145e430a0849b9bd2
Adham Ibrahim <37982706+adhamsi@users.noreply.github.com>
2025-07-25 15:32:52 -0400

    Implement HyKKT Ruiz Scaling (#317)
    * Added the permutation class from hykkt, along with tests.
    
    * HyKKT GPU Permutation Kernels (Attempt 2) (#333)
    
    * Added the permutation class from hykkt, along with tests.
    
    * refactor with base class for perm kernels
    
    * add kernel handler to Permutation class to call appropriate functions
    
    * add workspace parameter to test
    
    * gpu implementations
    
    * add PermutationHandler
    
    * test for more rows than cols
    
    * add documentation for permutation
    
    * remove use of linalgworkspace and update permutation constructor
    
    * mapIndex test
    
    * update signatures
    
    * memory handling and cosmetic changes
    
    * all functions on host except mapIdx
    
    * signature and fix leak
    
    * Apply pre-commmit fixes
    
    * remove unused variables
    
    * remove unused files and members, fix build
    
    * Apply pre-commmit fixes
    
    * use resolve types
    
    * Apply pre-commmit fixes
    
    ---------
    
    Co-authored-by: shakedregev <35384901+shakedregev@users.noreply.github.com>
    Co-authored-by: adhamsi <adhamsi@users.noreply.github.com>
    
    * set devImpl to null if no GPU backend
    
    * Added the permutation class from hykkt, along with tests.
    
    * add ruiz scaler and handler
    
    * refactor the interfaces and use existing MemoryHandler for reset
    
    * update interface
    
    * allocate/deallocate scaling vectors
    
    * remove handler as middleman
    
    * add empty cpu implementation and cmakelists
    
    * fix cmakelists
    
    * cpu implementation
    
    * hip implementation
    
    * use resolve matrix and vector types
    
    * cuda implementation
    
    * ruiz scaling test
    
    * fix bugs in kernel implementations
    
    * comments in implementation files
    
    * fix imports and leak
    
    * update comments
    
    * Doxygen reference and rename class
    
    * use object pointers and update all usage
    
    * better ruiz test
    
    * merge conflict
    
    * remove unused MemoryHandler
    
    * change long function names, file name capitalization, remove unused params
    
    * include issue
    
    * Apply pre-commmit fixes
    
    * error handling in constructor if no gpu support available
    
    * fix comment
    
    * error handling in constructor
    
    * reset workspace after each test
    
    * Check for J_tr
    
    ---------
    
    Co-authored-by: shakedregev <35384901+shakedregev@users.noreply.github.com>
    Co-authored-by: adhamsi <adhamsi@users.noreply.github.com>

---
+++ resolve/hykkt/ruiz/RuizScaling.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "RuizScalingKernelImpl.hpp"
+#include <resolve/Common.hpp>
+#include <resolve/MemoryUtils.hpp>
+#include <resolve/matrix/Csr.hpp>
+#include <resolve/utilities/logger/Logger.hpp>
+#include <resolve/vector/Vector.hpp>
+
+namespace ReSolve
+{
+  using index_type = ReSolve::index_type;
+  using real_type  = ReSolve::real_type;
+
+  namespace hykkt
+  {
+    class RuizScaling
+    {
+    public:
+      RuizScaling(index_type          n,
+                  index_type          total_n,
+                  memory::MemorySpace memspace);
+      ~RuizScaling();
+
+      void            addMatrixData(matrix::Csr* hes, matrix::Csr* jac, matrix::Csr* jac_tr);
+      void            addRhsData(vector::Vector* rhs_top, vector::Vector* rhs_bottom);
+      vector::Vector* getAggregateScalingVector() const;
+      void            scale(index_type num_iterations);
+
+    private:
+      index_type n_;       // Size of hessian (block [1,1])
+      index_type total_n_; // Total matrix size
+
+      // Matrix data
+      matrix::Csr*    hes_;        // Hessian matrix
+      matrix::Csr*    jac_;        // Jacobian matrix
+      matrix::Csr*    jac_tr_;     // Transposed Jacobian matrix
+      vector::Vector* rhs_top_;    // Top of the right-hand side vector
+      vector::Vector* rhs_bottom_; // Bottom of the right-hand side vector
+
+      vector::Vector* scaling_vector_;           // Scaling vector data for the current iteration
+      vector::Vector* aggregate_scaling_vector_; // Cumulative scaling vector
+
+      RuizScalingKernelImpl* kernelImpl_;
+
+      memory::MemorySpace memspace_;
+
+      void resetScaling();
+      void allocateWorkspace();
+      void deallocateWorkspace();
+    };
+  } // namespace hykkt
+} // namespace ReSolve
---
+++ resolve/hykkt/ruiz/RuizScalingKernelImpl.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <cmath>
+
+#include <resolve/Common.hpp>
+#include <resolve/MemoryUtils.hpp>
+#include <resolve/matrix/Csr.hpp>
+#include <resolve/vector/Vector.hpp>
+
+namespace ReSolve
+{
+  namespace hykkt
+  {
+
+    class RuizScalingKernelImpl
+    {
+    public:
+      using index_type = ReSolve::index_type;
+      using real_type  = ReSolve::real_type;
+
+      virtual ~RuizScalingKernelImpl() = default;
+
+      /**
+       * @brief Compute one iteration of Ruiz scaling
+       *
+       * @post scaling_vector[i] = 1 / sqrt(max_val) where max_val is the maximum absolute
+       *       value of the entries in the i-th row of the block matrix [hes jac^T; jac 0]
+       *
+       * @param[in] n_hes - Number of rows in the Hessian matrix.
+       * @param[in] n_total - Total number of rows in the 2x2 block system.
+       * @param[in] hes - Pointer to the Hessian matrix in CSR format.
+       * @param[in] jac - Pointer to the Jacobian matrix in CSR format.
+       * @param[in] jac_tr - Pointer to the transposed Jacobian matrix in CSR format.
+       * @param[out] scaling_vector - Scaling vector to be updated in-place.
+       */
+      virtual void adaptRowMax(index_type      n_hes,
+                               index_type      n_total,
+                               matrix::Csr*    hes,
+                               matrix::Csr*    jac,
+                               matrix::Csr*    jac_tr,
+                               vector::Vector* scaling_vector) = 0;
+
+      /**
+       * @brief Apply the scaling stored in scaling_vector to the matrices and rhs
+       *
+       * @post The left hand side is scaled from the left and right and the right hand side
+       *       is scaled from the right by the scaling_vector.
+       *
+       * @param[in] n_hes - Number of rows in the Hessian matrix.
+       * @param[in] n_total - Total number of rows in the 2x2 block system.
+       * @param[in] hes - Pointer to the Hessian matrix in CSR format to be updated in-place.
+       * @param[in] jac - Pointer to the Jacobian matrix in CSR format to be updated in-place.
+       * @param[in] jac_tr - Pointer to the transposed Jacobian matrix in CSR format to be updated in-place.
+       * @param[in] rhs_top - Right-hand side vector for the top block to be updated in-place.
+       * @param[in] rhs_bottom - Right-hand side vector for the bottom block to be updated in-place.
+       * @param[in,out] aggregate_scaling_vector - Cumulative scaling vector to be updated in-place.
+       * @param[in] scaling_vector - Scaling vector for the current iteration.
+       */
+      virtual void adaptDiagScale(index_type      n_hes,
+                                  index_type      n_total,
+                                  matrix::Csr*    hes,
+                                  matrix::Csr*    jac,
+                                  matrix::Csr*    jac_tr,
+                                  vector::Vector* rhs_top,
+                                  vector::Vector* rhs_bottom,
+                                  vector::Vector* aggregate_scaling_vector,
+                                  vector::Vector* scaling_vector) = 0;
+    };
+
+  } // namespace hykkt
+} // namespace ReSolve
