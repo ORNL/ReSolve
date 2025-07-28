15785ef3f442058b61f5dc0429a8169a40399355
superwhiskers <whiskerdev@protonmail.com>
2025-07-28 16:18:29 -0500

    address the remainder of the comments in #317 (#349)
    * address the left over comments in #317
    
    * Apply pre-commmit fixes
    
    ---------
    
    Co-authored-by: superwhiskers <superwhiskers@users.noreply.github.com>

--- resolve/hykkt/ruiz/RuizScaling.hpp
+++ resolve/hykkt/ruiz/RuizScaling.hpp
@@ -24,12 +24,14 @@ namespace ReSolve
 
       void            addMatrixData(matrix::Csr* hes, matrix::Csr* jac, matrix::Csr* jac_tr);
       void            addRhsData(vector::Vector* rhs_top, vector::Vector* rhs_bottom);
-      vector::Vector* getAggregateScalingVector() const;
+      vector::Vector* getAggregateScalingVector();
       void            scale(index_type num_iterations);
 
     private:
       index_type n_;       // Size of hessian (block [1,1])
       index_type total_n_; // Total matrix size
+
+      memory::MemorySpace memspace_; // The memory space we are working in
 
       // Matrix data
       matrix::Csr*    hes_;        // Hessian matrix
@@ -38,16 +40,12 @@ namespace ReSolve
       vector::Vector* rhs_top_;    // Top of the right-hand side vector
       vector::Vector* rhs_bottom_; // Bottom of the right-hand side vector
 
-      vector::Vector* scaling_vector_;           // Scaling vector data for the current iteration
-      vector::Vector* aggregate_scaling_vector_; // Cumulative scaling vector
+      vector::Vector scaling_vector_;           // Scaling vector data for the current iteration
+      vector::Vector aggregate_scaling_vector_; // Cumulative scaling vector
 
       RuizScalingKernelImpl* kernelImpl_;
 
-      memory::MemorySpace memspace_;
-
       void resetScaling();
-      void allocateWorkspace();
-      void deallocateWorkspace();
     };
   } // namespace hykkt
 } // namespace ReSolve
