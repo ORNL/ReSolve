964f73a9ea173c7be906a7af029f695e9236a29a
Slaven Peles <peless@ornl.gov>
2025-08-05 18:09:52 -0400

    Undo doxyfile template changes

--- renderDocs.sh
+++ renderDocs.sh
@@ -5,7 +5,7 @@ git submodule update --init --recursive
 pushd docs
 rm -rfd ./sphinx/_build &&
 sphinx-build . ./sphinx/_build &&
-doxygen ./doxygen/Doxyfile &&
+doxygen ./doxygen/Doxyfile.in &&
 # Host the docs locally with python
 python3 -m http.server --directory ./sphinx/_build
 popd
