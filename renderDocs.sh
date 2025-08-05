36022837ede8a6153109722d00bc2e81fa2e23a5
Slaven Peles <peless@ornl.gov>
2025-08-05 17:09:31 -0400

    Use correct Doxyfile

--- renderDocs.sh
+++ renderDocs.sh
@@ -5,7 +5,7 @@ git submodule update --init --recursive
 pushd docs
 rm -rfd ./sphinx/_build &&
 sphinx-build . ./sphinx/_build &&
-doxygen ./doxygen/Doxyfile.in &&
+doxygen ./doxygen/Doxyfile &&
 # Host the docs locally with python
 python3 -m http.server --directory ./sphinx/_build
 popd
