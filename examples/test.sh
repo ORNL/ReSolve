d5056b75f6ad78686b5f2ea7f06801316b3a65b1
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2025-08-20 15:21:09 -0400

    Passing matrices and rhs to resolve_consumer.

--- examples/test.sh
+++ examples/test.sh
@@ -10,6 +10,14 @@ echo ${ReSolve_DIR}
 export DATA_DIR=${2}
 echo ${DATA_DIR}
 
+# Defines the input matrices
+export MATRICES=${3}
+echo ${MATRICES}
+
+# Defines the input right hand sides
+export RHS=${4}
+echo ${RHS}
+
 # Locate source of the consumer test app
 export INSTALL_BUILD_CONSUME=${ReSolve_DIR}/share/examples/resolve_consumer
 echo ${INSTALL_BUILD_CONSUME}
@@ -18,7 +26,8 @@ mkdir -p ${INSTALL_BUILD_CONSUME}/build
 
 rm -rf ${INSTALL_BUILD_CONSUME}/build/* &&
 
-cmake -B ${INSTALL_BUILD_CONSUME}/build -S ${INSTALL_BUILD_CONSUME} -DRESOLVE_DATA=${DATA_DIR} -DReSolve_DIR=${ReSolve_DIR} &&
+cmake -B ${INSTALL_BUILD_CONSUME}/build -S ${INSTALL_BUILD_CONSUME} -DReSolve_DATA_DIR=${DATA_DIR} \
+  -DReSolve_MATRICES=${MATRICES} -DReSolve_RHS=${RHS} -DReSolve_DIR=${ReSolve_DIR} &&
 
 cmake --build ${INSTALL_BUILD_CONSUME}/build -- -j 12 &&
 
