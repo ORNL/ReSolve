5cbbdcc7666e57267bef7596804daa5c3f8c7883
shakedregev <shakedregev@users.noreply.github.com>
2025-11-13 22:41:50 +0000

    Apply pre-commmit fixes

--- cmake/FindSuiteSparse.cmake
+++ cmake/FindSuiteSparse.cmake
@@ -9,25 +9,27 @@ Author(s):
 - Cameron Rutherford <cameron.rutherford@pnnl.gov>
 
 ]]
-set(SUITESPARSE_MODULES
-  amd
-  colamd
-  klu
-  cholmod
-  suitesparseconfig)
+set(SUITESPARSE_MODULES amd colamd klu cholmod suitesparseconfig)
 
-find_library(SUITESPARSE_LIBRARY
-  NAMES
-  ${SUITESPARSE_MODULES}
-  PATHS
-  ${SUITESPARSE_DIR} $ENV{SUITESPARSE_DIR} ${SUITESPARSE_ROOT_DIR}
-  ENV LD_LIBRARY_PATH ENV DYLD_LIBRARY_PATH
-  PATH_SUFFIXES
-  lib/x86_64-linux-gnu lib64 lib)
+find_library(
+  SUITESPARSE_LIBRARY
+  NAMES ${SUITESPARSE_MODULES}
+  PATHS ${SUITESPARSE_DIR}
+        $ENV{SUITESPARSE_DIR}
+        ${SUITESPARSE_ROOT_DIR}
+        ENV
+        LD_LIBRARY_PATH
+        ENV
+        DYLD_LIBRARY_PATH
+  PATH_SUFFIXES lib/x86_64-linux-gnu lib64 lib
+)
 
 if(SUITESPARSE_LIBRARY)
   set(SUITESPARSE_LIBRARY CACHE FILEPATH "File path to Suitesparse library")
-  get_filename_component(SUITESPARSE_LIBRARY_DIR ${SUITESPARSE_LIBRARY} DIRECTORY CACHE "Suitesparse library directory")
+  get_filename_component(
+    SUITESPARSE_LIBRARY_DIR ${SUITESPARSE_LIBRARY} DIRECTORY CACHE
+    "Suitesparse library directory"
+  )
   message(STATUS "Found Suitesparse libraries in: " ${SUITESPARSE_LIBRARY_DIR})
   mark_as_advanced(SUITESPARSE_LIBRARY SUITESPARSE_LIBRARY_DIR)
   if(NOT SUITESPARSE_DIR)
@@ -38,17 +40,13 @@ if(SUITESPARSE_LIBRARY)
 endif()
 
 # Find SUITESPARSE header path and ensure all needed files are there
-find_path(SUITESPARSE_INCLUDE_DIR
-  NAMES
-  amd.h
-  colamd.h
-  klu.h
-  SuiteSparse_config.h
-  PATHS
-  ${SUITESPARSE_DIR} $ENV{SUITESPARSE_DIR} ${SUITESPARSE_ROOT_DIR} ${SUITESPARSE_LIBRARY_DIR}/..
-  PATH_SUFFIXES
-  include
-  include/suitesparse)
+find_path(
+  SUITESPARSE_INCLUDE_DIR
+  NAMES amd.h colamd.h klu.h SuiteSparse_config.h
+  PATHS ${SUITESPARSE_DIR} $ENV{SUITESPARSE_DIR} ${SUITESPARSE_ROOT_DIR}
+        ${SUITESPARSE_LIBRARY_DIR}/..
+  PATH_SUFFIXES include include/suitesparse
+)
 
 if(SUITESPARSE_LIBRARY)
   message(STATUS "Found Suitesparse include: ${SUITESPARSE_INCLUDE_DIR}")
@@ -88,6 +86,8 @@ else()
     )
   endif()
   if(SUITESPARSE_ROOT_DIR AND NOT SUITESPARSE_INCLUDE_DIR)
-    message(STATUS "Suitesparse include dir not found! Please provide correct path.")
+    message(
+      STATUS "Suitesparse include dir not found! Please provide correct path."
+    )
   endif()
 endif()
