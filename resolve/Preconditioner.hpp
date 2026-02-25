4bb1fac4f9b9f16916c79f60eacd8d892c69216b
JeffZ594 <JeffZ594@users.noreply.github.com>
2026-02-25 22:11:26 +0000

    Apply pre-commmit fixes

--- resolve/Preconditioner.hpp
+++ resolve/Preconditioner.hpp
@@ -38,7 +38,7 @@ namespace ReSolve
     virtual int          setup(matrix_type* A)                   = 0;
     virtual int          apply(vector_type* rhs, vector_type* x) = 0;
     virtual int          reset(matrix_type* /* A */);
-    virtual std::string  getSide(); // Gets the preconditioning side
+    virtual std::string  getSide();       // Gets the preconditioning side
     virtual matrix_type* getPrecMatrix(); // Get the preconditioner matrix
 
   private:
