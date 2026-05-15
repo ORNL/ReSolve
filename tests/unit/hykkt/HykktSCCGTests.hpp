151832388e0ab1f91154190831546cb09dff8b0e
Shaked Regev <35384901+shakedregev@users.noreply.github.com>
2026-05-15 09:58:06 -0400

    Update tests/unit/hykkt/HykktSCCGTests.hpp
    No commented out code.

--- tests/unit/hykkt/HykktSCCGTests.hpp
+++ tests/unit/hykkt/HykktSCCGTests.hpp
@@ -90,12 +90,6 @@ namespace ReSolve
         sccg.setup();
         sccg.solve();
 
-        // // //
-        // real_type* x0_output_data = x0.getData(0, memspace_);
-        // for (index_type i = 0; i < n; i++) {
-        //   std::cout << x0_output_data[i] << ", ";
-        // }
-        // // //
 
         TestStatus  status;
         std::string testname(__func__);
