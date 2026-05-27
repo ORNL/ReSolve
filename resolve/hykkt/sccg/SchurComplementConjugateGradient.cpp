a06f578ac822e4e33f6aff9349a5131b6515e7ce
Tamar DeWilde <tamard19@gmail.com>
2026-05-27 12:45:59 -0700

    update variable names to follow conventions

--- resolve/hykkt/sccg/SchurComplementConjugateGradient.cpp
+++ resolve/hykkt/sccg/SchurComplementConjugateGradient.cpp
@@ -13,20 +13,20 @@ namespace ReSolve
      *  @param m[in] - Dimension of inner system.
      *  @param choleskySolver[in] - Factorization of Hgamma to use for direct solve.
      *  @param memspace[in] - Memory space of incoming data and for computation.
-     *  @param matrixHandler[in] - Matrix handler for the selected backend.
-     *  @param vectorHandler[in] - Vector handler for the selected backend.
+     *  @param matrix_handler[in] - Matrix handler for the selected backend.
+     *  @param vector_handler[in] - Vector handler for the selected backend.
      */
     SchurComplementConjugateGradient::SchurComplementConjugateGradient(
         index_type          n,
         index_type          m,
         CholeskySolver*     choleskySolver,
         memory::MemorySpace memspace,
-        MatrixHandler&      matrixHandler,
-        VectorHandler&      vectorHandler)
+        MatrixHandler&      matrix_handler,
+        VectorHandler&      vector_handler)
       : n_(n),
         m_(m),
-        matrixhandler_(matrixHandler),
-        vectorhandler_(vectorHandler),
+        matrix_handler_(matrix_handler),
+        vector_handler_(vector_handler),
         choleskySolver_(choleskySolver),
         y_(m_),
         z_(m_),
@@ -103,37 +103,37 @@ namespace ReSolve
     {
       using namespace constants;
 
-      matrixhandler_.matvec(jc_tr_, x0_, &y_, &ONE, &ZERO, memspace_);
+      matrix_handler_.matvec(jc_tr_, x0_, &y_, &ONE, &ZERO, memspace_);
       choleskySolver_->solve(&z_, &y_);
-      matrixhandler_.matvec(jc_, &z_, &r_, &MINUS_ONE, &ONE, memspace_);
-      gam_i_ = vectorhandler_.dot(&r_, &r_, memspace_);
+      matrix_handler_.matvec(jc_, &z_, &r_, &MINUS_ONE, &ONE, memspace_);
+      gam_i_ = vector_handler_.dot(&r_, &r_, memspace_);
 
-      matrixhandler_.matvec(jc_tr_, &r_, &y_, &ONE, &ZERO, memspace_);
+      matrix_handler_.matvec(jc_tr_, &r_, &y_, &ONE, &ZERO, memspace_);
       choleskySolver_->solve(&z_, &y_);
-      matrixhandler_.matvec(jc_, &z_, &w_, &ONE, &ZERO, memspace_);
-      delta_ = vectorhandler_.dot(&w_, &r_, memspace_);
+      matrix_handler_.matvec(jc_, &z_, &w_, &ONE, &ZERO, memspace_);
+      delta_ = vector_handler_.dot(&w_, &r_, memspace_);
       alpha_ = gam_i_ / delta_;
 
       size_t i;
       for (i = 0; i < itmax_; i++)
       {
-        vectorhandler_.scal(beta_, &p_, memspace_);
-        vectorhandler_.axpy(ONE, &r_, &p_, memspace_);
-        vectorhandler_.scal(beta_, &s_, memspace_);
-        vectorhandler_.axpy(ONE, &w_, &s_, memspace_);
-        vectorhandler_.axpy(alpha_, &p_, x0_, memspace_);
+        vector_handler_.scal(beta_, &p_, memspace_);
+        vector_handler_.axpy(ONE, &r_, &p_, memspace_);
+        vector_handler_.scal(beta_, &s_, memspace_);
+        vector_handler_.axpy(ONE, &w_, &s_, memspace_);
+        vector_handler_.axpy(alpha_, &p_, x0_, memspace_);
         minalpha_ = -alpha_;
-        vectorhandler_.axpy(minalpha_, &s_, &r_, memspace_);
-        gam_i1_ = vectorhandler_.dot(&r_, &r_, memspace_);
+        vector_handler_.axpy(minalpha_, &s_, &r_, memspace_);
+        gam_i1_ = vector_handler_.dot(&r_, &r_, memspace_);
         if (sqrt(gam_i1_) < tol_)
         {
           printf("Convergence occured at iteration %d\n", i);
           break;
         }
-        matrixhandler_.matvec(jc_tr_, &r_, &y_, &ONE, &ZERO, memspace_);
+        matrix_handler_.matvec(jc_tr_, &r_, &y_, &ONE, &ZERO, memspace_);
         choleskySolver_->solve(&z_, &y_);
-        matrixhandler_.matvec(jc_, &z_, &w_, &ONE, &ZERO, memspace_);
-        delta_ = vectorhandler_.dot(&w_, &r_, memspace_);
+        matrix_handler_.matvec(jc_, &z_, &w_, &ONE, &ZERO, memspace_);
+        delta_ = vector_handler_.dot(&w_, &r_, memspace_);
         beta_  = gam_i1_ / gam_i_;
         gam_i_ = gam_i1_;
         alpha_ = gam_i_ / (delta_ - beta_ * gam_i_ / alpha_);
