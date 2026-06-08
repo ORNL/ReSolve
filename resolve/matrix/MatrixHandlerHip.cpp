1d0c07dbbed5edbddcefdc33af3e4e2a92f542be
Andrew Xu <andrewxu4321@gmail.com>
2026-06-08 21:36:50 +0000

    Remove automatic allocation from csc2csr()

--- resolve/matrix/MatrixHandlerHip.cpp
+++ resolve/matrix/MatrixHandlerHip.cpp
@@ -220,8 +220,7 @@ namespace ReSolve
     index_type error_sum = 0;
 
     rocsparse_status status;
-
-    A_csr->allocateMatrixData(memory::DEVICE);
+    
     index_type m   = A_csc->getNumColumns();
     index_type n   = A_csc->getNumRows();
     index_type nnz = A_csc->getNnz();
