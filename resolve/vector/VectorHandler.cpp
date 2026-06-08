2f5709e805ca17242ed36d6158db2cca68a709a1
Andrew Xu <andrewxu4321@gmail.com>
2026-06-08 22:35:43 +0000

    Add Vector::isAllocated(). This avoids checking whether a vector is allocated by calling getData(), which also requires the data to be updated to get a valid result.

--- resolve/vector/VectorHandler.cpp
+++ resolve/vector/VectorHandler.cpp
@@ -419,8 +419,8 @@ namespace ReSolve
   {
     assert(x->getSize() == y->getSize() && "Vectors must be the same size.");
     assert(x->getSize() == out->getSize() && "Vectors must be the same size.");
-    assert(x->getData(memspace) != nullptr && "Vector x data is null!");
-    assert(y->getData(memspace) != nullptr && "Vector y data is null!");
+    assert(x->isAllocated(memspace) && "Vector x data is null!");
+    assert(y->isAllocated(memspace) && "Vector y data is null!");
     assert(out->getData(memspace) != nullptr && "Vector out data is null!");
     using namespace ReSolve::memory;
     switch (memspace)
