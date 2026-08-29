df52248bfb5ca603b82b5a205310221661131555
andrewxu319 <andrewxu4321@gmail.com>
2026-08-29 16:52:52 -0400

    Add tests for all multTSMTTSM implementations

--- tests/unit/hykkt/HykktMBPCGTests.hpp
+++ tests/unit/hykkt/HykktMBPCGTests.hpp
@@ -84,7 +84,7 @@ namespace ReSolve
         else
         {
           b = new vector::Vector(n);
-          b->allocateAll(memspace_);
+          b->allocate(memspace_);
           vector_handler_.randomVector(b, -1.0, 1.0, memspace_);
         }
 
@@ -94,34 +94,14 @@ namespace ReSolve
         TestStatus  status;
         int num_fails = 0;
 
-        printf("\nTesting with diagonal scaling.\n");
-        // k = 1, 2, 4, 8
-        for (index_type k = 1; k <= 8; k *= 2)
-        {
-          if (k == 8) continue;
-          printf("\nk=%d\n", k);
-          hykkt::MultiBasisParallelConjugateGradient mbpcg(n, k, &matrix_handler_, &vector_handler_, memspace_);
-          mbpcg.setSolverTolerance(initial_tol, convergence_tol);
-          // mbpcg.setSolverItmax();
-
-          mbpcg.addMatrixInfo(A);
-          mbpcg.addVectorInfo(x, b);
-          mbpcg.diagonalScale();
-        
-          mbpcg.setup();
-          int converged = mbpcg.solve(); // 0 if converged, 1 if not
-          num_fails += (converged != 0);
-        }
-
-        printf("\nTesting with preconditioner.\n");
         PreconditionerIChol0 preconditioner(&matrix_handler_, &workspace_);
         preconditioner.setNumericBoost(numeric_boost_);
         preconditioner.setup(A);
 
+        printf("\nTesting with custom kernels, symmetric.\n");
         // k = 1, 2, 4, 8
         for (index_type k = 1; k <= 8; k *= 2)
         {
-          // if (k != 8) continue;
           printf("\nk=%d\n", k);
           preconditioner.setNumRhs(k);
           hykkt::MultiBasisParallelConjugateGradient mbpcg(n, k, &preconditioner, &matrix_handler_, &vector_handler_, memspace_);
@@ -131,7 +111,41 @@ namespace ReSolve
           mbpcg.addMatrixInfo(A);
           mbpcg.addVectorInfo(x, b);
           mbpcg.setup();
-          int converged = mbpcg.solve(); // 0 if converged, 1 if not
+          int converged = mbpcg.solve(hykkt::MultiBasisParallelConjugateGradient::MultTSMTTSMMode::CUSTOM_KERNEL_SYMMETRIC); // 0 if converged, 1 if not
+          num_fails += (converged != 0);
+        }
+
+        printf("\nTesting with custom kernels, asymmetric.\n");
+        // k = 1, 2, 4, 8
+        for (index_type k = 1; k <= 8; k *= 2)
+        {
+          printf("\nk=%d\n", k);
+          preconditioner.setNumRhs(k);
+          hykkt::MultiBasisParallelConjugateGradient mbpcg(n, k, &preconditioner, &matrix_handler_, &vector_handler_, memspace_);
+          mbpcg.setSolverTolerance(initial_tol, convergence_tol);
+          // mbpcg.setSolverItmax();
+
+          mbpcg.addMatrixInfo(A);
+          mbpcg.addVectorInfo(x, b);
+          mbpcg.setup();
+          int converged = mbpcg.solve(hykkt::MultiBasisParallelConjugateGradient::MultTSMTTSMMode::CUSTOM_KERNEL_ASYMMETRIC); // 0 if converged, 1 if not
+          num_fails += (converged != 0);
+        }
+
+        printf("\nTesting with cuBLAS/rocBLAS GEMM function.\n");
+        // k = 1, 2, 4, 8
+        for (index_type k = 1; k <= 8; k *= 2)
+        {
+          printf("\nk=%d\n", k);
+          preconditioner.setNumRhs(k);
+          hykkt::MultiBasisParallelConjugateGradient mbpcg(n, k, &preconditioner, &matrix_handler_, &vector_handler_, memspace_);
+          mbpcg.setSolverTolerance(initial_tol, convergence_tol);
+          // mbpcg.setSolverItmax();
+
+          mbpcg.addMatrixInfo(A);
+          mbpcg.addVectorInfo(x, b);
+          mbpcg.setup();
+          int converged = mbpcg.solve(hykkt::MultiBasisParallelConjugateGradient::MultTSMTTSMMode::GEMM_LIBRARY); // 0 if converged, 1 if not
           num_fails += (converged != 0);
         }
 
@@ -154,7 +168,7 @@ namespace ReSolve
 
       static constexpr real_type initial_tol = 1e-8;
       static constexpr real_type convergence_tol     = 1e-8;
-      static constexpr real_type numeric_boost_ = 0;
+      static constexpr real_type numeric_boost_ = 1e12; // Tuned for Fault_639. Adjust for other test matrices
     }; // class HykktMultiBasisParallelConjugateGradientTests
   } // namespace tests
 } // namespace ReSolve
--- tests/unit/hykkt/runHykktMBPCGTests.cpp
+++ tests/unit/hykkt/runHykktMBPCGTests.cpp
@@ -29,7 +29,6 @@
 template <typename WorkspaceType>
 void runTests(const std::string& backend, ReSolve::memory::MemorySpace memspace, ReSolve::tests::TestingResults& result)
 {
-// std::freopen("log.txt", "w", stdout);
   std::cout << "Running MBPCG tests on " << backend << " device:\n";
 
   WorkspaceType workspace;
@@ -41,157 +40,8 @@ void runTests(const std::string& backend, ReSolve::memory::MemorySpace memspace,
   std::string        source_dir = std::string(SOURCE_DIR);
 
   std::vector<std::string> matrix_names{
-    // "mhd4800b", // WARMUP
-    // "mhd4800b", // WARMUP
-    // "mhd4800b", // WARMUP
-    // "bcsstk18", // WARMUP
-
-    // "hood",
-    // "Fault_639",
-    // "2cubes_sphere",
-
-    // "1138_bus",
-    // "Chem97ZtZ",
-    // "sts4098",
-    // "bcsstk13",
-    // "bcsstk18",
-    // "torsion1",
-    // "shallow_water1",
-    // "Kuu",
-    // "cvxbqp1",
-    // "gridgena",
-    // "wathen120",
-    // "finan512",
-    // "thermomech_TC",
-    // "Pres_Poisson",
-    // "G2_circuit",
-    // "bundle1",
-    // "qa8fm",
-    // "cfd2",
-    "parabolic_fem",
-    // "ecology2",
-    // "tmt_sym",
-    // "G3_circuit",
-
-    // "cbuckle",
-    // "BenElechi1",
-    // "msdoor",
-    // "apache1",
-    // "x104",//
-    // "thread", //
-    // "shipsec8",
-    // "ship_001",
-    // "m_t1", //
-    // "bmwcra_1",
-    // "bmw7st_1",//
-    // "af_shell4",
-    // "nd6k",
-    // "olafu",
-    // "consph",
-
-    // "s3dkq4m2",
-    // "vanbody",//
-    // "raefsky4",
-    // "nasasrb",
-    // "ct20stif",
-    // "PFlow_742",//
-
-    // "cant",
-    // "offshore",
-
-    // "Dubcova3",
-    // "obstclae",
-    // "ship_003",
-    // "nd24k",
-    // "bcsstk38",
-    // "bcsstk17",
-    // "s2rmq4m1",
-
-    // "pwtk",
-    // "crankseg_2",
-    // "Fault_639",
-    // "ldoor",
-    // "boneS10",
-    // "Hook_1498",
-    // "bone010",
-    // "Flan_1565",
-
-    // "msc23052",//
-    // "msc10848",
-    // "msc04515",
-    // "bcsstk24",
-    // "bcsstk15",
-    // "Trefethen_20000",
-    // "Dubcova1",
-    // "ted_B",
-    // "minsurfo",
-    // "jnlbrng1",
-    // "audikw_1",//
-    // "t2dah_e",
-    // "nd12k",
-    // "nd3k",
-    // "fv3",
-    // "bodyy4",
-    // "aft01",
-
-    // "t3dl_e",
-    // "nasa1824",
-    // "msc01440",
-    // "msc00726",
-    // "bcsstm39",
-    // "nos3",
-    // "bcsstk11",
-    // "bcsstk06",
-    // "bcsstk04",
-    // "Trefethen_700",
-    // "mhd4800b",
-    // "Journals",
-
-    // "bcsstm26",
-    // "bcsstm22",
-    // "bcsstm20",
-    // "bcsstm19",
-    // "bcsstm11",
-    // "bcsstm09",
-    // "bcsstm08",
-    // "bcsstm06",
-    // "bcsstm05",
-    // "bcsstm02",
-    // "bcsstk22",
-    // "bcsstk03",
-    // "bcsstk01",
-    // "494_bus",
-    // "mesh3em5",
-    // "mesh1em6",
-    // "ex5",
-    // "nos1",
-
-    // "bcsstk34",
-    // "bcsstk28",
-    // "bcsstk25",
-    // "bcsstk10",
-    // "plbuckle",
-    // "msc01050"
-    // "thermomech_dM",
-    // "oilpan",
-    // "bundle_adj",
-    // "Geo_1438",
+    "Fault_639"
   };
-
-/*
-broken (maybe theres limits to how low the error can get)
-    "thermomech_dM",
-    "oilpan",
-    "bundle_adj",
-    "Geo_1438",
-    "denormal",
-    "m_t1"
-
-
-    queen, lfat5000, bloweybq, uni_chimerai2, trefethen_20
-
-    GENUINELY BROKEN, DONT EVEN TRY: LFAT5, LF10, nos4
-*/
 
   for (const std::string& matrix_name : matrix_names)
   {
@@ -203,7 +53,6 @@ broken (maybe theres limits to how low the error can get)
 
     std::cout << "\n";
   }
-    // std::fclose(stdout);
 }
 
 int main(int, char**)
